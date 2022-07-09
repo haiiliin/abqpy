dffa2b1c3921a56dea232cab6f9d099bb4b59f6c
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-07-09 13:13:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/haiiliin/abqpy",
     classifiers=[
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
