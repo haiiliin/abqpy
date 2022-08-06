5e8b560e2b4ec77f86ac54f69e1281c9f7eb463a
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-06 14:20:57 +0800

    Update to v2016a5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, WANG Hailin'
 author = 'WANG Hailin'
 
 # The full version, including alpha/beta/rc tags
-release = '2016a4'
+release = '2016a5'
 
 # -- General configuration ---------------------------------------------------
 
