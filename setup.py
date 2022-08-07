fdfcc8c96580e960cdb47221319197bc29cb059c
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-07 17:50:58 +0800

    Update to v2017a6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="abqpy",
-    version="2017a5",
+    version="2017a6",
     author="WANG Hailin",
     author_email="hailin.wang@connect.polyu.hk",
     description="Type hints for Abaqus/Python scripting",
