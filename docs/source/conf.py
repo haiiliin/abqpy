aefc4654459b4a4506beb6b9de68fef07dd1786c
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-08 21:23:27 +0800

    Update version to v2020a8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, WANG Hailin'
 author = 'WANG Hailin'
 
 # The full version, including alpha/beta/rc tags
-release = '2019a7'
+release = '2020a8'
 
 # -- General configuration ---------------------------------------------------
 
