4fe925c5fd183dfa792dbb57463cea743260a155
Hailin Wang <2011133@tongji.edu.cn>
2022-08-09 01:22:40 +0800

    Update version v2016a9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2022, WANG Hailin'
 author = 'WANG Hailin'
 
 # The full version, including alpha/beta/rc tags
-release = '2016a8'
+release = '2016v9'
 
 # -- General configuration ---------------------------------------------------
 
