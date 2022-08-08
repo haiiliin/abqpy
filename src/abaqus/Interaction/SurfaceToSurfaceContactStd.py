4efc881048b360665750f9a2424da7ec51f21004
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-08 17:11:00 +0800

    Add handedness and normalAdjustment argument for SurfaceToSurfaceContactStd

--- src/abaqus/Interaction/SurfaceToSurfaceContactStd.py
+++ src/abaqus/Interaction/SurfaceToSurfaceContactStd.py
@@ -171,6 +171,15 @@ class SurfaceToSurfaceContactStd(Interaction):
     #: contact property CohesiveBehavior option specifies use.
     bondingSet: Region = None
 
+    #: A SymbolicConstant specifying the bolt handedness formulation. Possible values are RIGHT
+    #: and LEFT. The default value is RIGHT.
+    handedness: SymbolicConstant = RIGHT
+
+    #: A SymbolicConstant specifying the bolt normal adjustment formulation for all secondary
+    #: nodes. Possible values are UNIFORM AXIAL COMPONENT and LOCATION DEPENDENT. The default
+    #: value is UNIFORM AXIAL COMPONENT.
+    normalAdjustment: SymbolicConstant = NONE
+
     def __init__(
         self,
         name: str,
@@ -204,6 +213,8 @@ class SurfaceToSurfaceContactStd(Interaction):
         clearanceRegion: Region = None,
         surfaceSmoothing: SymbolicConstant = NONE,
         bondingSet: Region = None,
+        handedness: SymbolicConstant = RIGHT,
+        normalAdjustment: SymbolicConstant = None,
     ):
         """This method creates a SurfaceToSurfaceContactStd object.
 
@@ -322,6 +333,13 @@ class SurfaceToSurfaceContactStd(Interaction):
         bondingSet
             A :py:class:`~abaqus.Region.Region.Region` object specifying the secondary node sub-set for bonding, used only when the
             contact property CohesiveBehavior option specifies use.
+        handedness
+            A SymbolicConstant specifying the bolt handedness formulation. Possible values are RIGHT
+            and LEFT. The default value is RIGHT.
+        normalAdjustment
+            A SymbolicConstant specifying the bolt normal adjustment formulation for all secondary
+            nodes. Possible values are UNIFORM AXIAL COMPONENT and LOCATION DEPENDENT. The default
+            value is UNIFORM AXIAL COMPONENT.
 
         Returns
         -------
