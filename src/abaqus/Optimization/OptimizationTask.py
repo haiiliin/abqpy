c5f3661a6593202b24af0385d59c0190c151c658
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-09 13:29:56 +0800

    Remove TopologyMillingControl for V2016-V2021
    (cherry picked from commit 376962787a4e06acda636904942f92afb18c3931)

--- src/abaqus/Optimization/OptimizationTask.py
+++ src/abaqus/Optimization/OptimizationTask.py
@@ -34,7 +34,6 @@ from .StepOptionArray import StepOptionArray
 from .TopologyCyclicSymmetry import TopologyCyclicSymmetry
 from .TopologyDemoldControl import TopologyDemoldControl
 from .TopologyMemberSize import TopologyMemberSize
-from .TopologyMillingControl import TopologyMillingControl
 from .TopologyOverhangControl import TopologyOverhangControl
 from .TopologyPlanarSymmetry import TopologyPlanarSymmetry
 from .TopologyPointSymmetry import TopologyPointSymmetry
@@ -1719,59 +1718,6 @@ class OptimizationTask(OptimizationTaskBase):
         """
         self.geometricRestrictions[name] = geometricRestriction = TopologyMemberSize(
             name, region, maxThickness, minThickness, separation, sizeRestriction
-        )
-        return geometricRestriction
-
-    def TopologyMillingControl(
-        self,
-        name: str,
-        millingDirections: tuple,
-        region: Region,
-        csys: int = None,
-        millingCheckRegion: SymbolicConstant = MILLING_REGION,
-        radius: float = None,
-    ) -> TopologyMillingControl:
-        """This method creates a TopologyMillingControl object.
-
-        .. note:: 
-            This function can be accessed by:
-
-            .. code-block:: python
-
-                mdb.models[name].optimizationTasks[name].TopologyMillingControl
-
-        Parameters
-        ----------
-        name
-            A String specifying the geometric restriction repository key.
-        millingDirections
-            A tuple of VertexArray objects of length 2 specifying the milling directions. Each point
-            can be specified through a tuple of coordinates instead of through a ConstrainedSketchVertex.
-        region
-            A :py:class:`~abaqus.Region.Region.Region` object specifying the region to which the geometric restriction is applied.
-        csys
-            None or a DatumCsys object specifying the local coordinate system of the
-            **millingDirections**. If **csys** = None, the global coordinate system is used. When this
-            member is queried, it returns an Int indicating the identifier of the DatumCsys. The
-            default value is None.
-        millingCheckRegion
-            The SymbolicConstant MILLING_REGION or a Region object specifying the milling check
-            region. If the value is MILLING_REGION, the value of **region** is used as both the
-            milling control region and the milling check region. The default value is
-            MILLING_REGION.
-        radius
-            A Float specifying the radius for the collision check during the removal of the elements
-            for the milling criteria.
-
-        Returns
-        -------
-        TopologyMillingControl
-            A :py:class:`~abaqus.Optimization.TopologyMillingControl.TopologyMillingControl` object.
-        """
-        self.geometricRestrictions[
-            name
-        ] = geometricRestriction = TopologyMillingControl(
-            name, millingDirections, region, csys, millingCheckRegion, radius
         )
         return geometricRestriction
 
