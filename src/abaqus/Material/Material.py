0ed0661a2049b06d24e55377b7592fb9894b2026
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-09 13:16:13 +0800

    Add CrushStress material

--- src/abaqus/Material/Material.py
+++ src/abaqus/Material/Material.py
@@ -47,6 +47,7 @@ from .Plastic.Concrete.ConcreteDamagedPlasticity import ConcreteDamagedPlasticit
 from .Plastic.Creep.Creep import Creep
 from .Plastic.CriticalStateClay.ClayPlasticity import ClayPlasticity
 from .Plastic.CrushableFoam.CrushableFoam import CrushableFoam
+from .Plastic.CrushStress.CrushStress import CrushStress
 from .Plastic.DruckerPrager.Extended.DruckerPrager import DruckerPrager
 from .Plastic.DruckerPrager.ModifiedCap.CapPlasticity import CapPlasticity
 from .Plastic.Metal.CastIron.CastIronPlasticity import CastIronPlasticity
@@ -521,6 +522,40 @@ class Material(MaterialBase):
             table, hardening, temperatureDependency, dependencies
         )
         return self.crushableFoam
+
+    def CrushStress(
+        self,
+        crushStressTable: tuple[tuple[float, ...]],
+        temperatureDependency: Boolean = OFF,
+        dependencies: int = 0,
+    ):
+        """This method creates a CrushStress object.
+
+        .. note::
+            This function can be accessed by:
+
+            .. code-block:: python
+
+                mdb.models[name].materials[name].CrushStress
+                session.odbs[name].materials[name].CrushStress
+
+        Parameters
+        ----------
+        crushStressTable
+            A sequence of sequences of Floats specifying the items described below.
+        temperatureDependency
+            A Boolean specifying whether the data depend on temperature. The default value is OFF.
+        dependencies
+            An Int specifying the number of field variable dependencies. The default value is 0.
+
+        Returns
+        -------
+            A CrushStress object.
+        """
+        self.crushStress = CrushStress(
+            crushStressTable, temperatureDependency, dependencies
+        )
+        return self.crushStress
 
     def Damping(
         self,
