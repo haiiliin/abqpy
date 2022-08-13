8466c6cc905e749600a4224f19bd243256f4c118
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-13 16:47:16 +0800

    Update docs
    (cherry picked from commit 824769dbe4957e1ae3ad498dd332f9c871234ee4)

--- src/abaqus/_OptionsBase.py
+++ src/abaqus/_OptionsBase.py
@@ -46,7 +46,7 @@ class _CopyOptionsBase(_OptionsBase):
             An object from which values are to be copied. If other arguments are also supplied to setValues,
             they will override the values in **options**. The default value is None.
         kwargs
-            Keyword arguments.
+            The keyword arguments.
         """
         if options is not None:
             for key in self.get_user_attributes():
