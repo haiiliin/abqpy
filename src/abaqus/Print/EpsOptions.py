ceb147a1e91f9686a5c0f3e5a4ed4717b30db59f
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-13 14:30:23 +0800

    Implement Print Options
    (cherry picked from commit e90ce7341c891d8eca68c52a46884a45fdc993f6)

--- src/abaqus/Print/EpsOptions.py
+++ src/abaqus/Print/EpsOptions.py
@@ -15,6 +15,35 @@ class EpsOptions:
 
             session.epsOptions
     """
+    #: The SymbolicConstant SIZE_ON_SCREEN or a pair of Floats specifying the width and height
+    #: of the image in the units specified by **units**. Possible numeric values are **imageSize**
+    #: ≥≥ (*minWidth*, **minHeight**). The default value is SIZE_ON_SCREEN.Note:The minimum value
+    #: of width and height (*minWidth* and **minHeight**) is 10 mm (approximately 0.4 inches).
+    imageSize: typing.Union[SymbolicConstant, float] = SIZE_ON_SCREEN
+
+    #: A SymbolicConstant specifying the units of the **imageSize** argument. This argument is
+    #: ignored if **imageSize** is SIZE_ON_SCREEN. Possible values are INCHES and MM. The default
+    #: value is INCHES.
+    units: SymbolicConstant = INCHES
+
+    #: A SymbolicConstant specifying the resolution of the image in dots per inch (dpi). The
+    #: **resolution** can be DPI_1200 only if **imageFormat** = VECTOR. Possible values are DPI_75,
+    #: DPI_150, DPI_300, DPI_450, DPI_600, and DPI_1200. The default value is DPI_150.
+    resolution: SymbolicConstant = DPI_150
+
+    #: A SymbolicConstant specifying the PostScript font substitution rules to be applied.
+    #: Possible values are PS_ALWAYS, PS_IF_AVAILABLE, and AS_DISPLAYED. The default value is
+    #: PS_IF_AVAILABLE.
+    fontType: SymbolicConstant = PS_IF_AVAILABLE
+
+    #: A SymbolicConstant specifying how the viewport display will be represented. Possible
+    #: values are VECTOR and RASTER. The default value is VECTOR.
+    imageFormat: SymbolicConstant = VECTOR
+
+    #: A SymbolicConstant specifying how fine the shading of curved surfaces will be for vector
+    #: images. Possible values are EXTRA COARSE, COARSE, MEDIUM, FINE, and EXTRA FINE. The default
+    #: value is MEDIUM.
+    shadingQuality: SymbolicConstant = MEDIUM
 
     def setValues(
         self,
@@ -56,9 +85,12 @@ class EpsOptions:
 
         Raises
         ------
-        RangeError
-            - If either element of **imageSize** is out of range:
-              RangeError: imageSize must be SIZE_ON_SCREEN or a sequence of 2 Floats >= (*minWidth*,
-            **minHeight**)
+        RangeError: imageSize must be SIZE_ON_SCREEN or a sequence of 2 Floats >= (*minWidth*, **minHeight**)
+            If either element of **imageSize** is out of range.
         """
-        ...
+        self.imageSize = imageSize
+        self.units = units
+        self.resolution = resolution
+        self.fontType = fontType
+        self.imageFormat = imageFormat
+        self.shadingQuality = shadingQuality
