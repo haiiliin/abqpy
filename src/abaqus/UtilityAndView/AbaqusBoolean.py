7169a395feb4c421acc11014dccb53cf16c51a1d
Hailin Wang <2011133@tongji.edu.cn>
2022-08-13 12:26:09 +0800

    Update AbaqusBoolean.py

--- src/abaqus/UtilityAndView/AbaqusBoolean.py
+++ src/abaqus/UtilityAndView/AbaqusBoolean.py
@@ -3,7 +3,8 @@ class AbaqusBoolean(int):
     pass an AbaqusBoolean object to the Python repr() function, the function returns the
     text without quotes. In effect, the text is the variable that, by convention, refers to
     the AbaqusBoolean object.
-    An :py:class:`~abaqus.UtilityAndView.AbaqusBoolean.AbaqusBoolean` object has a value of 0 or 1, and an AbaqusBoolean object can be tested
+    An :py:class:`~abaqus.UtilityAndView.AbaqusBoolean.AbaqusBoolean` object has a value of 0 or 1,
+    and an AbaqusBoolean object can be tested
     in an if statement. You can use an AbaqusBoolean object as an argument to a method in
     place of 1 or 0. Conversely, you can pass a 0 or a 1 to an Abaqus Scripting Interface
     method that expects a Boolean argument, and the 0 or 1 will be coerced to the
