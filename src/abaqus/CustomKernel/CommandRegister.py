14cb372cabf2dc0ac57bee8df4934a6cf640d3f0
Hailin Wang <2011133@tongji.edu.cn>
2022-08-13 23:49:32 +0800

    Implement Registered Objects
    (cherry picked from commit a6d74596a4412b1364a2aa241274a7896e4f4a62)

--- src/abaqus/CustomKernel/CommandRegister.py
+++ src/abaqus/CustomKernel/CommandRegister.py
@@ -1,3 +1,4 @@
+
 class CommandRegister:
     """This class allows you to derive a general class that can be queried from the GUI and is
     capable of notifying the GUI when the contents of the class change.
@@ -26,4 +27,4 @@ class CommandRegister:
         CommandRegister
             A :py:class:`~abaqus.CustomKernel.CommandRegister.CommandRegister` object.
         """
-        ...
+        pass
--- src/abaqus/CustomKernel/RegisteredTuple.py
+++ src/abaqus/CustomKernel/RegisteredTuple.py
@@ -1,7 +1,7 @@
 from .CommandRegister import CommandRegister
 
 
-class RegisteredTuple(CommandRegister):
+class RegisteredTuple(CommandRegister, tuple):
     """This class allows you to create a tuple that can be queried from the GUI and is capable
     of notifying the GUI when the contents of any of the tuple's members change.
     The RegisteredTuple object is derived from the CommandRegister object.
@@ -38,4 +38,5 @@ class RegisteredTuple(CommandRegister):
 
     def Methods(self):
         """The RegisteredTuple object supports the same methods as a standard Python list object."""
+        # TODO: Implement this method
         ...
