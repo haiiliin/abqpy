8c310c0e00e3c9b1d4c1c431cf72c8ba15816174
Hailin Wang <2011133@tongji.edu.cn>
2022-08-14 21:50:10 +0800

    Resolve conflicts

--- src/abaqus/Sketcher/ConstrainedSketchGeometry/ConstrainedSketchGeometryModel.py
+++ src/abaqus/Sketcher/ConstrainedSketchGeometry/ConstrainedSketchGeometryModel.py
@@ -50,16 +50,9 @@ class ConstrainedSketchGeometryModel(ConstrainedSketchBase):
 
     def ArcByCenterEnds(
         self,
-<<<<<<< HEAD
-        center: tuple[float, ...],
-        point1: tuple[float, ...],
-        point2: tuple[float, ...],
-=======
         center: typing.Tuple[float, ...],
         point1: typing.Tuple[float, ...],
         point2: typing.Tuple[float, ...],
-        direction: SymbolicConstant,
->>>>>>> 94bc5bb (Use typing annotations to support Python 3.8 and less)
     ):
         """This method constructs an arc using a center point and two vertices. The Arc object is
         added to the geometry repository of the ConstrainedSketch object. The arc is created in
