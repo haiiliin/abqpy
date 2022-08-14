3925f81131c402845c1a385ecd062a7623445ef6
Hailin Wang <2011133@tongji.edu.cn>
2022-08-14 21:41:07 +0800

    Remove MeanFieldHomogenization

--- src/abaqus/Material/Material.py
+++ src/abaqus/Material/Material.py
@@ -15,7 +15,6 @@ from .Gasket.GasketMembraneElastic import GasketMembraneElastic
 from .Gasket.GasketThicknessBehavior import GasketThicknessBehavior
 from .Gasket.GasketTransverseShearElastic import GasketTransverseShearElastic
 from .MaterialBase import MaterialBase
-from .Multiscale.MeanFieldHomogenization import MeanFieldHomogenization
 from .Others.Acoustic.AcousticMedium import AcousticMedium
 from .Others.Electromagnetic.Dielectric import Dielectric
 from .Others.Electromagnetic.ElectricalConductivity import ElectricalConductivity
@@ -3832,46 +3831,3 @@ class Material(MaterialBase):
         RangeError
         """
         return DamageInitiation()
-
-    def MeanFieldHomogenization(
-        self,
-        angleSubdivision: int = None,
-        formulation: SymbolicConstant = MT,
-        isotropization: SymbolicConstant = ALLISO,
-        uniformMatrixStrain: SymbolicConstant = NO,
-    ):
-        """This method creates a MeanFieldHomogenization object.
-
-        Notes
-        -----
-            This function can be accessed by:
-
-            .. code-block:: python
-
-                mdb.models[name].materials[name].MeanFieldHomogenization
-                session.odbs[name].materials[name].MeanFieldHomogenization
-
-        Parameters
-        ----------
-        angleSubdivision
-            An Int specifying the number of angle increments used for the discretization of the
-            orientation space.
-        formulation
-            A SymbolicConstant specifying the type of homogenization model. Possible values are MT,
-            REUSS, VOIGT, INVERSED_MT, BALANCED, and UNSPECIFIED. The default value is MT.
-        isotropization
-            A SymbolicConstant specifying the type of isotropization method. Possible values are
-            ALLISO, EISO, and PISO. The default value is ALLISO.
-        uniformMatrixStrain
-            A SymbolicConstant specifying whether the average strain in the matrix is uniform across
-            all pseudo-grains. Possible values are NO and YES. The default value is NO.
-
-        Returns
-        -------
-            A MeanFieldHomogenization object.
-
-        Raises
-        ------
-        RangeError
-        """
-        return MeanFieldHomogenization()
