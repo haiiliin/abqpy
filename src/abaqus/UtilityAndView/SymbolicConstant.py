d91344918e0f5cfe045fd1654600c6bd3a18280c
Diogo Rossi <rossi.diogo@gmail.com>
2022-08-15 01:02:09 -0300

    Update SymbolicConstants class

--- src/abaqus/UtilityAndView/SymbolicConstant.py
+++ src/abaqus/UtilityAndView/SymbolicConstant.py
@@ -1,4 +1,6 @@
-class SymbolicConstant:
+from __future__ import annotations
+
+class SymbolicConstant(str):
     """The SymbolicConstant object represents a string in a way that can be stored in a replay
     file and used as an argument to Abaqus Scripting Interface methods and functions. By
     convention the string representation of the SymbolicConstant object is the same as its
@@ -21,7 +23,7 @@ class SymbolicConstant:
             from abaqusConstants import *
     """
 
-    def __init__(self, text: str):
+    def __init__(self, name: str, scdId:int = -1) -> None:
         """The SymbolicConstant method creates a SymbolicConstant object.
 
         .. note:: 
@@ -42,4 +44,53 @@ class SymbolicConstant:
         SymbolicConstant
             A :py:class:`~abaqus.UtilityAndView.SymbolicConstant.SymbolicConstant` object.
         """
+        for n in name:
+            if n not in [chr(s) for s in list(range(65,91)) + list(range(48,58))] + ['_']:
+                raise ValueError(f'SymbolicConstant name {name} may only contain upper case, digit or underscore')
+        self.name = name
+
+    def __copy__(self) -> SymbolicConstant:
         ...
+    
+    def __getstate__(self) -> str:
+        return self.name
+    
+    def __hash__(self) -> int:
+        ...
+    
+    def __lt__(self, other: Any) -> bool:
+        """Sorting method; True if self is < other"""
+        return self < other
+    
+    def __reduce__(self) -> tuple:  # known return case of __reduce__
+        ...
+    
+    def __repr__(self) -> str: 
+        return self.getText()
+    
+    def __setstate__(self, *args) -> None:
+        ...
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    def getId(self) -> int:
+        ...
+    
+    def getText(self) -> str:
+        return self.name
+    
+    @staticmethod 
+    def __new__(cls, *args, **kargs) -> SymbolicConstant:
+        ...
+    
+    @classmethod
+    def _addToCache(cls, *args, **kwargs):  # real signature unknown
+        """
+        Helper method for __new__
+        Also used by AbaqusBoolean.__new__
+        """
+        pass
+
+    __weakref__ = property(lambda self: object(), lambda self, v: None, lambda self: None)  # default
+    """list of weak references to the object (if defined)"""
