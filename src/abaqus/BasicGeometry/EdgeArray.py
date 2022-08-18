3002f5873bd92678030648e55880df0c5163c79f
Hailin Wang <2011133@tongji.edu.cn>
2022-08-18 16:34:20 +0800

    Update format of some code blocks and others
    (cherry picked from commit eb0e72d9d9c48c63278200dfecfcc7811b670c74)

--- src/abaqus/BasicGeometry/EdgeArray.py
+++ src/abaqus/BasicGeometry/EdgeArray.py
@@ -111,10 +111,15 @@ class EdgeArray(typing.List[Edge]):
         ----------
         coordinates
             A sequence of a sequence of floats, where each sequence of floats describes the **X**-,
-            **Y**-, and **Z**-coordinates of a
-            point.`r=e.getClosest(coordinates=((20.0,20.0,10.0),(-1.0, -15.0, 15),))``r.keys()``[0,
-            1]``r[0]``(mdb.models['Model-1'].parts['Part-1'].edges[3],
-              (15.7090625762939, 20.0, 10.0))`
+            **Y**-, and **Z**-coordinates of a point.
+            
+            .. code-block:: python
+            
+                >>> r=e.getClosest(coordinates=((20.0, 20.0, 10.0), (-1.0, -15.0, 15), ))
+                >>> r.keys()
+                [0, 1]
+                >>> r[0]
+                (mdb.models['Model-1'].parts['Part-1'].edges[3], (15.7090625762939, 20.0, 10.0))
         searchTolerance
             A double specifying the distance within which the closest object must lie. The default
             value is half of the parent part/instance size.
