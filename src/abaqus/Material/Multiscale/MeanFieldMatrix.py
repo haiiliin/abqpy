0d2ea36a8e36c79e0c3eedc6ab9e7a0e15bcf48c
Hailin-Wang <2011133@tongji.edu.cn>
2022-08-19 12:59:29 +0800

    Move the analysis keywords to class notes
    (cherry picked from commit fec6cb11d8f237840d97b83cb83fe8c8e6eca0ce)
    
    # Conflicts:
    #	src/abaqus/Interaction/FluidInflator.py
    #	src/abaqus/Interaction/SlidingFormulationAssignment.py
    #	src/abaqus/Interaction/SurfaceFrictionAssignment.py
    #	src/abaqus/Interaction/SurfaceVertexCriteriaAssignment.py
    #	src/abaqus/Material/Multiscale/MeanFieldHomogenization.py
    #	src/abaqus/Material/Multiscale/MeanFieldInclusion.py
    #	src/abaqus/Material/Multiscale/MeanFieldMatrix.py
    #	src/abaqus/Material/Multiscale/MeanFieldVoid.py
    #	src/abaqus/Material/Plastic/CrushStress/CrushStress.py

---
+++ src/abaqus/Material/Multiscale/MeanFieldMatrix.py
@@ -0,0 +1,60 @@
+class MeanFieldMatrix:
+    """The MeanFieldMatrix object specifies the matrix property.
+
+    .. note::
+        This object can be accessed by:
+
+        .. code-block:: python
+
+            import material
+            mdb.models[name].materials[name].constituents[name]
+            import odbMaterial
+            session.odbs[name].materials[name].constituents[name]
+
+        The corresponding analysis keywords are:
+
+        - CONSTITUENT
+
+    """
+
+    def __init__(
+        self, name: str, material: str = "", isotropizationCoefficient: float = None
+    ):
+        """This method creates a MeanFieldMatrix object.
+
+        .. note::
+            This function can be accessed by:
+
+            .. code-block:: python
+
+                mdb.models[name].materials[name].meanFieldHomogenization.MeanFieldMatrix
+                session.odbs[name].materials[name].meanFieldHomogenization.MeanFieldMatrix
+
+        Parameters
+        ----------
+        name
+            A String specifying the constituent repository key.
+        material
+            A String specifying the name of the material.
+        isotropizationCoefficient
+            A Float specifying the factor used for scaling the Plastic strain of the constituent
+            when calculating the isotropic part of the tangent.
+
+        Returns
+        -------
+            A MeanFieldMatrix object.
+
+        Raises
+        ------
+        RangeError
+        """
+        ...
+
+    def setValues(self):
+        """This method modifies the MeanFieldMatrix object.
+
+        Raises
+        ------
+        RangeError
+        """
+        ...
