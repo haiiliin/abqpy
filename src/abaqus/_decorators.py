281c2543209473ff7202a20ca5528168c28ba1b1
Hailin Wang <2011133@tongji.edu.cn>
2022-08-21 00:56:49 +0800

    Move links to Abaqus documentation to the note environment
    (cherry picked from commit 3ddf487637fd2e50275c3d9c817ab6306d2e0644)

--- src/abaqus/_decorators.py
+++ src/abaqus/_decorators.py
@@ -140,7 +140,8 @@ class AbaqusDoc:
         if not docstring:
             return docstring
         link, link_with_label = cls._class_or_module_link(type, class_or_module_name, prefix)
-        return docstring.rstrip() + '\n\n' + ' ' * (0 if type == 'module' else 4) + link_with_label
+        return (docstring.rstrip() + '\n\n' + ' ' * (0 if type == 'module' else 4) +
+                '.. note::\n' + ' ' * (4 if type == 'module' else 8) + link_with_label)
 
     @classmethod
     def _add_link_in_method_or_function_docstring(
@@ -175,7 +176,9 @@ class AbaqusDoc:
             return docstring
         link, link_with_label = cls._method_or_function_link(type, class_or_module_name,
                                                              method_or_function_name, prefix)
-        return re.sub(r'(\n\s+?)(Parameters\n\s+----------)', r'\1' + link_with_label + r'\1\2', docstring)
+        return re.sub(r'(\n\s+?)(Parameters\n\s+----------)',
+                      r'\1' + '.. note::\n' + ' ' * (8 if type == 'function' else 12) + link_with_label + r'\1\2',
+                      docstring)
 
     @classmethod
     def add_link_in_class_docstring(
