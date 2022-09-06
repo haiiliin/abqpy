30e6ebbbe4f292d9e3132feb79a8681619c25b89
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-06 16:17:52 -0300

    add some missing main modules
    (cherry picked from commit b35089247380f88b1109153a8a4b44d7718fd79f)

---
+++ src/job.py
@@ -0,0 +1,10 @@
+from odbAccess import AnalyticSurfaceProfile
+from abaqus.Odb.AnalyticSurfaceSegment import AnalyticSurfaceSegment
+from abaqus.Odb.HistoryPoint import HistoryPoint
+from abaqus.Job.Job import Job
+from odbAccess import Odb
+from odbAccess import isUpgradeRequiredForOdb
+from odbAccess import openOdb
+from odbAccess import upgradeOdb
+# from odbAccess import closeOdb
+# from odbAccess import truncateOdb
