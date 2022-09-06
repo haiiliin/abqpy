8395e1535b560aa91c8aea9544ecca4e24972506
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-06 16:17:52 -0300

    add some missing main modules
    (cherry picked from commit b35089247380f88b1109153a8a4b44d7718fd79f)

---
+++ src/optimization.py
@@ -0,0 +1,16 @@
+from xyPlot import AreaStyle 
+from xyPlot import LineStyle 
+from xyPlot import QuantityType 
+from regionToolset import Region 
+from sketch import Sketch 
+from sketch import SketchTransform 
+from xyPlot import SymbolStyle 
+from xyPlot import TextStyle 
+from xyPlot import XYData 
+from xyPlot import XYDataFromFile 
+from xyPlot import XYDataFromFreeBody 
+from xyPlot import XYDataFromHistory 
+from xyPlot import XYDataFromPath 
+from xyPlot import XYDataFromShellThickness 
+from xyPlot import xyDataListFromField 
+# inspected from Abaqus cli
