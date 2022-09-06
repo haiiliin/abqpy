d146647e635262496e5ad63c41d62be9494a6e7e
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-06 18:05:44 -0300

    Remove `positional-only` arguments to prevent fail
    The syntax was giving error with flake8
    
    (cherry picked from commit ac1c3775ad0f747e63f5dd33c4c7b94996940f41)
    (cherry picked from commit 374ade1d29019b1c1e604f088114c299af49549e)

--- src/sketch.py
+++ src/sketch.py
@@ -1,7 +1,7 @@
 class FeatureSketch:
     ...
 
-def Sketch(self, sheetSize: float = 200, gridSize: float = 5, /) -> FeatureSketch:
+def Sketch(self, sheetSize: float = 200, gridSize: float = 5) -> FeatureSketch:
     ...
 
 def SketchTransform():
