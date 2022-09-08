ce3e1259111eeaa32bfb2190745777ecc5e42e9f
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-08 10:15:11 -0300

    Revert "Modify tests"
    As long as we don't need to submit to abaqus in the tests
    The modifications are not necessary.
    
    (cherry picked from commit 642d0e26c38e8eca004f4af6bba3ac8628436d10)

--- tests/test_odb.py
+++ tests/test_odb.py
@@ -1,41 +1,10 @@
-import sys, os
-# Execute only if in Python 3.x interpreter
-if sys.version_info[0] > 2:
-    # Check which folder the test are running from and then
-    # add the `src` folder to PATH for testing. Also change
-    # the work directory to `temp` to run Abaqus command in it.
-    folder = os.path.basename(os.getcwd())
-    if folder == 'temp':
-        sys.path.insert(0,os.path.abspath('../../src'))
-    elif folder == 'tests':
-        sys.path.insert(0,os.path.abspath('../src'))
-        os.chdir('temp')
-    else:
-        sys.path.insert(0,os.path.abspath('./src'))
-        os.chdir('tests/temp')
-    
-    #This check is needed to run from `pytest` framework
-    if sys.argv[0] != __file__:
-        sys.argv.insert(0,__file__)
-
+import os, sys
+sys.path.insert(0,os.path.abspath('./src'))
 from abaqus import *
 from abaqusConstants import *
 from driverUtils import *
 import numpy as np
-try:
-    import pytest # type: ignore
-except ImportError:
-    # define `pytest` to use in Abaqus interpreter
-    # because it doesn't have a `pytest` module
-    class pytest:
-        @staticmethod
-        def raises(error):
-            class EnterExit:
-                def __enter__(self):
-                    pass
-                def __exit__(self,typ,val,tb):
-                    pass
-            return EnterExit()
+import pytest
 
 
 def test_odb():
@@ -53,6 +22,3 @@ def test_odb():
         data = np.array(dataList[0])
         if data.size > 0:
             np.savetxt('data.csv', data, header='time,U3', delimiter=',', comments='')
-
-if sys.version_info[0] < 3: # run the test in Abaqus
-    test_odb()
