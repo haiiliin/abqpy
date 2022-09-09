f73e94548465615541cc60c8088600e6ac344734
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-09-09 13:04:07 +0800

    Auto update the suggested version when publishing the release (#1107)
    * Auto update the suggest version when publishing the release
    
    * Not update release drafter in the main branch
    
    * Update update-suggested-version.py
    
    * Update publish-release.yml
    
    * Update update-suggested-version.py
    
    * Update publish-release.yml
    
    * Update publish-release.yml
    
    * Update update-suggested-version.yml
    
    (cherry picked from commit 1bcf9806d62150e2b1ba4bb49e9a7898a55e4609)

---
+++ .github/update-suggested-version.py
@@ -0,0 +1,25 @@
+"""
+Run this file by `python update-suggested-version.py "20xx.x.x"` to update the version in
+README.md, README-zh-cn.md, and docs/source/getting_started.rst.
+"""
+import os
+import re
+import sys
+
+
+current_version = sys.argv[1]
+if current_version.startswith("v"):
+    current_version = current_version[1:]
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir("..")
+
+for file in ['README.md', 'README-zh-cn.md', 'docs/source/getting_started.rst']:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    pattern = r'abqpy(=+?)20\d\d\.\d+?\.\d+?'
+    repl = f'abqpy\g<1>{current_version}'
+    content = re.sub(pattern, repl, content)
+    
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(content)
