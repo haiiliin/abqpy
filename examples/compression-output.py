0b5d79e97b06a0013791676621fdbfc42c2e7315
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-09 00:16:45 -0300

    include lines in the example script and comment
    (cherry picked from commit 499a75f1a0f3c1f37620ee4fb19a3eea0c1461ad)

--- examples/compression-output.py
+++ examples/compression-output.py
@@ -8,6 +8,9 @@ executeOnCaeStartup()
 # Open output database
 odb = session.openOdb('Job-1.odb')
 
+# Show the output database in viewport
+session.viewports['Viewport: 1'].setValues(displayedObject=odb)
+
 # Extract output data
 dataList = session.xyDataListFromField(odb=odb, outputPosition=NODAL, 
                                        variable=(('U', NODAL, ((COMPONENT, 'U3'),)),),
