31578431a18787cb6e98bd53ee60a136e6d33d64
Hailin Wang <2011133@tongji.edu.cn>
2022-09-12 00:33:45 +0800

    Add argparse sample

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         'Anaconda': 'https://anaconda.org/haiiliin/abqpy',
         'Read the Docs': 'https://readthedocs.org/projects/abqpy',
     },
-    use_scm_version = {
+    use_scm_version={
         "root": ".",
         "relative_to": __file__,
         "local_scheme": "node-and-date",
@@ -46,4 +46,5 @@ setuptools.setup(
                 'mesh', 'meshEdit', 'methodCallback', 'monitorManager', 'odbAccess', 'odbConnectorBehavior',
                 'odbMaterial', 'odbSection', 'part', 'redentABQ', 'section', 'symbolicConstants', 'textRepr',
                 'upgradeScript', 'visualization'],
+    scripts=['bin/abqpy'],
 )
