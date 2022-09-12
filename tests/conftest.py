29a72f3c34b5232b64523b8ccc7b7c9102d13940
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-09-12 23:36:49 +0800

    Use pytest_configure to insert the source code to sys.path before executing pytest (#1215)
    * Use pytest_configure to insert the source code to sys.path before executing pytest
    
    * Update conftest.py
    
    (cherry picked from commit 15ce288fe81654934434aedb2decbb6fdfdba8de)

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+"""
+conftest.py: sharing fixtures across multiple files. https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files.
+"""
+import os
+import sys
+import pytest
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    """Allow plugins and conftest files to perform initial configuration.
+
+    This hook is called for every plugin and initial conftest file after command line options have been parsed.
+
+    After that, the hook is called for other conftest files as they are imported.
+
+    Args:
+        config (pytest.Config): The pytest config object.
+    """
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, os.path.abspath('../src'))
