haiiliin/abqpy

commit cf417e3e7cd6f23fb1e9079252690b2b591e1877

Use an independent package `abqpy` to store `non-abaqus-related` utils (#1241)
* Create abqpy package

* Use abqpy.abaqus.run_abaqus

* Move _decorators.py

* Refactor code using decorators

* Optimize Imports by PyCharm

* Rename run_abaqus to run

* Update __init__.py

* Move abqpy to src

src/abqpy/decorators.py
@@ -3,7 +3,7 @@ import typing
 from pathlib import Path
 
 try:
-    from ._version import version as _default_version
+    from abaqus._version import version as _default_version
 except ImportError:
     _default_version = '2022.0.0-unknown'
 

commit d33740f78215a8e9a69aa74bbbf0a37a3e714d32

Fix bugs of decorators introduced in #1241

src/abqpy/decorators.py
@@ -3,7 +3,7 @@ import typing
 from pathlib import Path
 
 try:
-    from abaqus._version import version as _default_version
+    from ._version import version as _default_version
 except ImportError:
     _default_version = '2022.0.0-unknown'
 