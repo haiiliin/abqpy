059bd88067b6a541c32e056c66d84828f8129430
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-13 22:34:48 -0300

    add doc string and type annotations

--- src/abqpy/abaqus.py
+++ src/abqpy/abaqus.py
@@ -2,7 +2,21 @@ import os
 import sys
 
 
-def run(cae=True, exit_after=True):
+def run(cae: bool = True, exit_after: bool = True) -> None:
+    """Runs Abaqus command in system's CLI
+    
+    This function uses the top level script file to run the Abaqus
+    command, and the arguments passed to it
+    
+    Parameters
+    ----------
+    cae : bool, optional
+        Wether or not to use `abaqus cae` command or
+        `abaqus python`, by default True
+    exit_after : bool, optional
+        Wether to exit of the Python3 interpreter
+        after calling Abaqus, by default False
+    """
     abaqus = os.environ.get("ABAQUS_BAT_PATH", "abaqus")
     filePath = os.path.abspath(sys.modules['__main__'].__file__)
     args = " ".join(sys.argv[1:])
