0a8bd2ebf9763ebc677447ef3999665beceebd89
Diogo <rossi.diogo@gmail.com>
2022-09-14 00:08:29 -0300

    Correct `run()` function always running `abaqus cae`, regardless of the called method (#1303)
    * delete temp folder
    
    * Modify `run()` to check the use of Abaqus CAE
    
    * add doc string and type annotations
    
    * fix annotations and doc string of functions
    
    Format doc strings and annotations of functions `session.openOdb`
    and `odbAccess.openOdb`
    
    (cherry picked from commit 3c75c5a095123a8d5cd425d0e55dd123000b32c1)
    
    # Conflicts:
    #	src/abaqus/__init__.py
    #	src/abqpy/abaqus.py
    #	src/odbAccess.py

--- src/abqpy/abaqus.py
+++ src/abqpy/abaqus.py
@@ -2,7 +2,25 @@ import os
 import sys
 
 
+<<<<<<< HEAD
 def run(exit_after=False):
+=======
+def run(cae: bool = True, exit_after: bool = True) -> None:
+    """Runs Abaqus command in system's CLI
+    
+    This function uses the top level script file to run the Abaqus
+    command, and the arguments passed to it
+    
+    Parameters
+    ----------
+    cae : bool, optional
+        Wether or not to use `abaqus cae` command or
+        `abaqus python`, by default True
+    exit_after : bool, optional
+        Wether to exit of the Python3 interpreter
+        after calling Abaqus, by default False
+    """
+>>>>>>> 3c75c5a (Correct `run()` function always running `abaqus cae`, regardless of the called method (#1303))
     abaqus = os.environ.get("ABAQUS_BAT_PATH", "abaqus")
     filePath = os.path.abspath(sys.modules['__main__'].__file__)
     args = " ".join(sys.argv[1:])
@@ -14,6 +32,9 @@ def run(exit_after=False):
         filePath = str(filePath).replace(".ipynb", ".py")
     except (FileNotFoundError, ImportError, Exception):
         pass
-    os.system(f"{abaqus} cae noGUI={filePath} -- {args}")
+    if cae:
+        os.system(f"{abaqus} cae noGUI={filePath} -- {args}")
+    else:
+        os.system(f"{abaqus} python {filePath} {args}")
     if exit_after:
         sys.exit(0)
