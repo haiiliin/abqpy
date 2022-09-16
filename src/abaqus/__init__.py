4394540f7c14c492f42cf8d47760ba9058539c5f
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-16 10:06:17 -0300

    way to not run model "twice" (python3 and abaqus)

--- src/abaqus/__init__.py
+++ src/abaqus/__init__.py
@@ -1,3 +1,4 @@
+import sys
 import abqpy.abaqus
 from .Canvas.Highlight import *
 from .Mdb.Mdb import Mdb as AbaqusMdb
@@ -36,3 +37,11 @@ backwardCompatibility = BackwardCompatibility()
 
 YES = SymbolicConstant('YES')
 NO = SymbolicConstant('NO')
+
+# check if in debug mode and run
+gettrace = getattr(sys, 'gettrace', None)
+exit_after = False
+if gettrace is None or not gettrace():
+    exit_after = True
+
+abqpy.abaqus.run(cae=True, exit_after=exit_after)
--- src/odbAccess.py
+++ src/odbAccess.py
@@ -1,4 +1,13 @@
+import sys
 from abaqus.Odb.OdbCommands import *
 from abaqus.UtilityAndView.BackwardCompatibility import BackwardCompatibility
 
 backwardCompatibility = BackwardCompatibility()
+
+# check if in debug mode and run
+gettrace = getattr(sys, 'gettrace', None)
+exit_after = False
+if gettrace is None or not gettrace():
+    exit_after = True
+
+abqpy.abaqus.run(cae=True, exit_after=exit_after)
