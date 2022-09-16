1496fb95919c66a0744e88d26cb2a32f2642665d
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-16 10:42:05 -0300

    simplify `__init__` module

--- src/abaqus/__init__.py
+++ src/abaqus/__init__.py
@@ -1,34 +1,13 @@
 import sys
 import abqpy.abaqus
 from .Canvas.Highlight import *
-from .Mdb.Mdb import Mdb as AbaqusMdb
+from .Mdb.Mdb import Mdb
 from .Odb.Odb import Odb
-from .Session.Session import Session as AbaqusSession
+from .Session.Session import Session
 from .UtilityAndView.BackwardCompatibility import BackwardCompatibility
 from .UtilityAndView.SymbolicConstant import SymbolicConstant
 from .UtilityAndView.abaqusConstants import Boolean, OFF
 from .UtilityAndView.User import *
-
-
-class Mdb(AbaqusMdb):
-
-    def __init__(self, pathName: str = ""):
-        super().__init__(pathName)
-
-    def save(self):
-        abqpy.abaqus.run()
-
-    def saveAs(self, pathName: str):
-        abqpy.abaqus.run()
-
-
-class Session(AbaqusSession):
-
-    def openOdb(self, name: str, path: str = "", readOnly: Boolean = OFF) -> Odb:
-        self.odbs[name] = odb = Odb(name)
-        abqpy.abaqus.run()
-        return odb
-
 
 session = Session()
 mdb = Mdb()
