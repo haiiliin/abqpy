fb0c34994cb67afade99d1228d969083d3da79a3
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-09-18 01:22:01 +0800

    Do not run the abaqus command in debug mode (#1501)
    * way to not run model "twice" (python3 and abaqus)
    
    * simplify `__init__` module
    
    * add empty line at the end
    
    * update `OdbCommands` to not run abaqus
    
    * correct error when import `abqpy`
    
    * remove `odbAccess` from `caeModules`
    
    * moving checks to `abqpy.abaqus.run`
    
    * Remove unused imports
    
    * adding an `Exception` to catch all kinds of errors
    
    * modify to handle `pytest`
    
    * Add `ABQPY_DEBUG` environment
    
    * test_odb does not need to handle SystemExit anymore
    
    * Add a default viewport for `session`
    
    * Remove print messages
    
    * Fix typo errors
    
    * Do not run abaqus command at all if in debug mode
    
    * Remove unused imports
    
    * Remove unused imports
    
    * Update abaqus.py
    
    * Add `ABQPY_MAKE_DOCS` environment variable to indicate if it is building sphinx docs
    
    Co-authored-by: Diogo Rossi <rossi.diogo@gmail.com>
    (cherry picked from commit a2cd44cccd537e09024b7774cac1212e57db6e92)
    
    # Conflicts:
    #	src/abaqus/__init__.py

--- tests/test_odb.py
+++ tests/test_odb.py
@@ -3,24 +3,22 @@ from abaqusConstants import *
 from driverUtils import *
 import visualization
 import numpy as np
-import pytest
 
 
 def test_odb():
     executeOnCaeStartup()
 
-    with pytest.raises(SystemExit):
-        # Open output database
-        odb = session.openOdb('Job-1.odb')
-        
-        # Show the output database in viewport
-        session.viewports['Viewport: 1'].setValues(displayedObject=odb)
-    
-        # Extract output data
-        dataList = session.xyDataListFromField(odb=odb, outputPosition=NODAL,
-                                               variable=(('U', NODAL, ((COMPONENT, 'U3'),)),),
-                                               nodeSets=('INSTANCE.SET-TOP', ))
-    
-        data = np.array(dataList[0])
-        if data.size > 0:
-            np.savetxt('data.csv', data, header='time,U3', delimiter=',', comments='')
+    # Open output database
+    odb = session.openOdb('Job-1.odb')
+
+    # Show the output database in viewport
+    session.viewports['Viewport: 1'].setValues(displayedObject=odb)
+
+    # Extract output data
+    dataList = session.xyDataListFromField(odb=odb, outputPosition=NODAL,
+                                           variable=(('U', NODAL, ((COMPONENT, 'U3'),)),),
+                                           nodeSets=('INSTANCE.SET-TOP',))
+
+    data = np.array(dataList[0])
+    if data.size > 0:
+        np.savetxt('data.csv', data, header='time,U3', delimiter=',', comments='')
