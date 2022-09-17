24ed59f298cb14a8db7b451364b1672c91ca748a
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-09-17 19:02:25 +0800

    Add Parameter classes

--- src/abqpy/template.py
+++ src/abqpy/template.py
@@ -1,11 +1,78 @@
 import importlib.util
+import numbers
 import os
-import typing
+
+import toml
+from typing import Any, Dict, List, Optional, Union, Type, NamedTuple
 
 from jinja2 import Template
 
 
-def available_templates() -> typing.List[str]:
+class Parameter(NamedTuple):
+    """A parameter for a template."""
+    #: The name of the parameter.
+    name: str
+    #: The type of the parameter.
+    type: Type = str
+    #: The default value of the parameter.
+    default: Any = None
+    #: The description of the parameter.
+    description: str = ''
+    #: The minimum value of the parameter.
+    min: Optional[numbers.Number] = None
+    #: The maximum value of the parameter.
+    max: Optional[numbers.Number] = None
+
+
+class StringParameter(Parameter):
+    """A string parameter for a template."""
+    type = str
+    default = ''
+
+
+class NumberParameter(Parameter):
+    """A number parameter for a template."""
+    type = numbers.Number
+    #: The default value of the parameter.
+    default: numbers.Number
+    #: The minimum value of the parameter.
+    min: Optional[numbers.Number] = None
+    #: The maximum value of the parameter.
+    max: Optional[numbers.Number] = None
+
+
+class IntegerParameter(NumberParameter):
+    """An integer parameter for a template."""
+    type = int
+    #: The default value of the parameter.
+    default: int = 0
+    #: The minimum value of the parameter.
+    min: Optional[int] = None
+    #: The maximum value of the parameter.
+    max: Optional[int] = None
+
+
+class BooleanParameter(Parameter):
+    """A boolean parameter for a template."""
+    type = bool
+    #: The default value of the parameter.
+    default: bool = False
+    min = False
+    max = True
+
+
+class FloatParameter(NumberParameter):
+    """A float parameter for a template."""
+    type = float
+    #: The default value of the parameter.
+    default: float = 0.0
+    #: The minimum value of the parameter.
+    min: Optional[float] = None
+    #: The maximum value of the parameter.
+    max: Optional[float] = None
+
+
+def available_templates() -> List[str]:
     """Return a list of available templates.
 
     Returns
@@ -18,42 +85,41 @@ def available_templates() -> typing.List[str]:
 
 
 class ScriptTemplate(Template):
-    _vars: typing.Dict[str, typing.Any]
+    params: List[Parameter] = []
 
-    def __new__(cls, tmp: typing.Union[os.PathLike, str, Template], **kwargs):
-        _vars = {}
-        if os.path.exists(tmp):
-            obj = super().__new__(cls, open(tmp, 'r', encoding='utf-8').read())
-        elif tmp in available_templates():
-            module = importlib.import_module(f'abqpy.templates.{tmp}')
-            obj = super().__new__(cls, module.template)
-            _vars.update(module.defaults)
-        else:
-            obj = super().__new__(cls, tmp)
-        obj._vars = _vars
-        obj._vars.update(kwargs)
+    def __new__(
+        cls,
+        source: Union[os.PathLike, str, Template],
+        config: Union[
+            List[Parameter],
+            Dict[str, Union[Template, Dict[str, Any]]],
+            os.PathLike, str,
+        ] = None,
+    ):
+        if os.path.exists(source):
+            source = open(source, 'r', encoding='utf-8').read()
+        elif source in available_templates():
+            source = importlib.import_module(f'abqpy.templates.{source}').template
+        obj = super().__new__(cls, source)
+        if os.path.exists(config) and os.path.isfile(config):
+            if config.endswith('.toml'):
+                config = toml.load(config)
+            elif config.endswith('.json'):
+                import json
+                with open(config, 'r', encoding='utf-8') as f:
+                    config = json.load(f)
+        if isinstance(config, list):
+            obj.params = config
+        elif isinstance(config, dict):
+            obj.params = []
+            for name, param in config.items():
+                if isinstance(param, Parameter):
+                    obj.params.append(param)
+                elif isinstance(param, dict):
+                    obj.params.append(Parameter(name, **param))
+                else:
+                    raise TypeError(f'Invalid parameter type: {type(param)}')
         return obj
-
-    def __init__(self, tmp: typing.Union[os.PathLike, str, Template], **kwargs):
-        """Get a template object.
-
-        Parameters
-        ----------
-        tmp : str or PathLike
-            The name of the template to use, or the path to the template file, or a string of the template,
-            or a Template object.
-        **kwargs
-            The parameters to render the template.
-
-        Returns
-        -------
-        Template
-            The template object.
-        """
-        if tmp in available_templates():
-            module = importlib.import_module(f'abqpy.templates.{tmp}')
-            self._vars.update(module.defaults)
-        self._vars.update(kwargs)
 
     def render(self, **kwargs) -> str:
         """Render the template.
@@ -68,10 +134,9 @@ class ScriptTemplate(Template):
         str
             The rendered template.
         """
-        self._vars.update(kwargs)
-        return super().render(**self._vars)
+        return super().render(**kwargs)
 
-    def write(self, file: typing.Union[os.PathLike, str], **kwargs):
+    def write(self, file: Union[os.PathLike, str], **kwargs):
         """Write the rendered template to a file.
 
         Parameters
@@ -92,4 +157,5 @@ def test_existing_templates():
 
 def test_render():
     """Test the render function."""
-    print(ScriptTemplate('COMPRESSION', width=2, length=2, height=2).render())
+    template = ScriptTemplate('compression', 'templates/compression.toml')
+    print(template.render(width=2, length=2, height=2))
