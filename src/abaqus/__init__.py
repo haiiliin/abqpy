05702cbd6d709e4df47faece50831cfc1a08ffb0
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-09-18 02:13:11 +0800

    Resolve conflicts

--- src/abaqus/__init__.py
+++ src/abaqus/__init__.py
@@ -8,30 +8,6 @@ from .UtilityAndView.SymbolicConstant import SymbolicConstant
 from .UtilityAndView.abaqusConstants import Boolean, OFF
 from .UtilityAndView.User import *
 
-<<<<<<< HEAD
-
-class Mdb(AbaqusMdb):
-
-    def __init__(self, pathName: str = ""):
-        super().__init__(pathName)
-
-    def save(self):
-        super().save()
-
-    def saveAs(self, pathName: str):
-        abqpy.abaqus.run()
-
-
-class Session(AbaqusSession):
-
-    def openOdb(self, name: str, path: str = "", readOnly: Boolean = OFF) -> Odb:
-        self.odbs[name] = odb = Odb(name)
-        abqpy.abaqus.run()
-        return odb
-
-
-=======
->>>>>>> a2cd44cc (Do not run the abaqus command in debug mode (#1501))
 session = Session()
 mdb = Mdb()
 
