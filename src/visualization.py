a448cff2b4470d84009ce6c7eb3942dec3fb680e
Diogo Rossi <rossi.diogo@gmail.com>
2022-09-17 15:16:13 -0300

    add blank line

--- src/visualization.py
+++ src/visualization.py
@@ -2,4 +2,4 @@ from abaqus.Property.PlyStackPlot import OdbPlyStackPlot
 from abaqus.Odb.OdbCommands import *
 from abaqus import session
 
-session.Viewport(name='Viewport: 1')
+session.Viewport(name="Viewport: 1")
