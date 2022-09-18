c510e29e82f52a41188fac9509c7f853347e51f7
Diogo <rossi.diogo@gmail.com>
2022-09-18 00:36:40 -0300

    Move line with `run()` function to before `import abaqus` (#1566)
    move line with `run()` function
    
    possible solution to correct issue in running odbAccess (`cae=False`)
    before importing `abaqus` (witch runs `cae=True`)
    
    (cherry picked from commit 1284d6bed2489f67f5c1ee5e6b52a3593d98390b)

--- src/odbAccess.py
+++ src/odbAccess.py
@@ -1,8 +1,8 @@
 import abqpy.abaqus
 
+abqpy.abaqus.run(cae=False)
+
 from abaqus.Odb.OdbCommands import *
 from abaqus.UtilityAndView.BackwardCompatibility import BackwardCompatibility
 
 backwardCompatibility = BackwardCompatibility()
-
-abqpy.abaqus.run(cae=False)
