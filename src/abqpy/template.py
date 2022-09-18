f76865ed4b62fd16f6fb319f69cc348304d50738
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-09-18 17:37:05 +0800

    Improve Templates Implementations (#1584)
    * Move templates to src/abqpy
    
    * Include package data
    
    * Use `.tmpl` as the template suffix
    
    * Rename _CompressionTemplate to CompressionTemplate
    
    * Update template docs
    
    * Remove warning in template docs
    
    * Move templates back to the top-level project directory
    
    TODO: It is a temporary solution
    
    * Update template.py
    
    * Support user conf option
    
    * CompressionTemplate support user argument
    
    * Add Example Usage
    
    * Update template.py
    
    * Rename toml files
    
    * Update template.py
    
    * Update docs
    
    * Update template.py
    
    * Update template.py
    
    (cherry picked from commit 4e0cc477046923adfe2e5c5565248b6e5a4fee1c)

--- src/abqpy/template.py
+++ src/abqpy/template.py
@@ -1,28 +1,79 @@
+import json
 import os
-from typing import Any, Dict, Union, Type
+from typing import Dict, Union, Type
 import textwrap
 
 import toml
 from jinja2 import Template
 
+__all__ = [
+    'CompressionTemplate',
+    'ScriptTemplate',
+]
+
+
+def load_json_or_toml(path: Union[str, Dict]) -> Dict:
+    """Load a json or toml file."""
+    if isinstance(path, dict):
+        return path
+    if not os.path.exists(path):
+        raise FileNotFoundError(f'File {path} does not exist.')
+    if path.endswith('.json'):
+        with open(path, encoding='utf-8') as f:
+            return json.load(f)
+    elif path.endswith('.toml'):
+        with open(path, encoding='utf-8') as f:
+            return toml.load(f)
+    else:
+        raise ValueError(f'Unknown file type: {path}')
+
 
 class ScriptTemplate(Template):
     """A template class for Abaqus script.
+
+    Examples
+    --------
+    >>> from abqpy.template import ScriptTemplate
+    >>> template = ScriptTemplate(template='template.py', config='config.toml', params='params.toml')
+    >>> template.write('script.py')
+    >>> template = ScriptTemplate(template='template.py', config='config.toml')
+    >>> template.write('script.py', param1=1, param2=2)
     """
-    _params: Dict[str, Dict[str, Union[str, int, float, bool]]]
+    #: Parameter config
+    _config: Dict[str, Dict[str, Union[str, int, float, bool]]]
+    #: User parameters config
+    _params: Dict[str, Union[str, int, float, bool]]
+    #: Template source
+    _template_source: str
+    #: Parameter config source
+    _config_source: str
+    #: User config source
+    _params_source: str
     #: A list of parameters required by the template.
-    parameters = property(lambda self: list(self._params))
+    keys = property(lambda self: list(self._config))
     #: The default parameters.
-    defaults = property(lambda self: {name: param['default'] for name, param in self._params.items()})
+    defaults = property(lambda self: {name: param['default'] for name, param in self._config.items()})
     #: The types of the parameters.
-    types = property(lambda self: {name: param['type'] for name, param in self._params.items()})
+    types = property(lambda self: {name: param['type'] for name, param in self._config.items()})
     #: The descriptions of the parameters.
-    descriptions = property(lambda self: {name: param['description'] for name, param in self._params.items()})
+    descriptions = property(lambda self: {name: param['description'] for name, param in self._config.items()})
+    #: Template source
+    template_source = property(lambda self: self._template_source)
+    #: Parameter config source
+    config_source = property(lambda self: self._config_source)
+    #: User config source
+    params_source = property(lambda self: self._params_source)
 
     def __new__(
         cls,
-        source: Union[os.PathLike, str, Template],
+        template: Union[
+            os.PathLike, str,
+        ],
         config: Union[
+            Dict[str, Dict[str, Union[str, int, float, bool]]],
+            os.PathLike, str,
+        ] = None,
+        params: Union[
             Dict[str, Dict[str, Union[str, int, float, bool]]],
             os.PathLike, str,
         ] = None,
@@ -31,28 +82,29 @@ class ScriptTemplate(Template):
 
         Parameters
         ----------
-        source : str or PathLike or Template
+        template : str or PathLike or Template
             The template source.
         config : str or PathLike or dict, optional
-            The config file or dict, by default None
+            The config file (a json or toml file) or dict, by default None
+        params : str or PathLike or dict, optional
+            The user params config file (a json or toml file) or dict, by default None
         """
-        if os.path.exists(source) and os.path.isfile(config):
-            source = open(source, 'r', encoding='utf-8').read()
-        obj = super().__new__(cls, source)
+        if os.path.exists(template) and os.path.isfile(config):
+            template = open(template, 'r', encoding='utf-8').read()
+        obj = super().__new__(cls, template)
+        obj._template_source = template
+
+        # Read the config file
         if os.path.exists(config) and os.path.isfile(config):
-            if config.endswith('.toml'):
-                config = toml.load(config)
-            elif config.endswith('.json'):
-                import json
-                with open(config, 'r', encoding='utf-8') as f:
-                    config = json.load(f)
-        obj._params = {}
-        if isinstance(config, dict):
-            for name, param in config.items():
-                if isinstance(param, Dict):
-                    obj._params[name] = dict(name=name, **param)
-                else:
-                    raise TypeError(f'Invalid parameter type: {type(param)}')
+            obj._config_source = open(config, 'r', encoding='utf-8').read()
+        obj._config = load_json_or_toml(config) if config else {}
+        for name in obj._config.keys():
+            obj._config[name]['name'] = name
+
+        # Read the user config file
+        if os.path.exists(params) and os.path.isfile(params):
+            obj._params_source = open(params, 'r', encoding='utf-8').read()
+        obj._params = load_json_or_toml(params) if params else {}
         return obj
 
     def check(self, correct_bounds=True, **kwargs):
@@ -76,19 +128,20 @@ class ScriptTemplate(Template):
             If the parameters are invalid.
         """
         for name, param in kwargs.items():
-            if name not in self._params:
+            if name not in self._config:
                 raise ValueError(f'Invalid parameter: {name}')
-            if 'min' in self._params[name] and param < self._params[name]['min']:
+            if 'min' in self._config[name] and param < self._config[name]['min']:
                 if correct_bounds:
-                    kwargs[name] = self._params[name]['min']
+                    kwargs[name] = self._config[name]['min']
                 else:
                     raise ValueError(f'Parameter {name} is too small: {param}')
-            if 'max' in self._params[name] and param > self._params[name]['max']:
+            if 'max' in self._config[name] and param > self._config[name]['max']:
                 if correct_bounds:
-                    kwargs[name] = self._params[name]['max']
+                    kwargs[name] = self._config[name]['max']
                 else:
                     raise ValueError(f'Parameter {name} is too large: {param}')
         params = self.defaults
+        params.update(self._params)
         params.update(kwargs)
         return params
 
@@ -121,49 +174,75 @@ class ScriptTemplate(Template):
             f.write(self.render(**kwargs))
 
 
-def template_doc(cls: Type['_CompressionTemplate']):
+def template_doc(cls: Type['CompressionTemplate']):
     """Generate the docstring for the template class."""
     obj = cls()
     attr_docstrings = []
-    parameters, types, defaults, descriptions = obj.parameters, obj.types, obj.defaults, obj.descriptions
-    for var in parameters:
-        attr_docstrings.append(f'.. confval:: {var}\n'
-                               f'    :type: {types[var]}, defaults to {defaults[var]}\n\n'
-                               f'    {descriptions[var]}')
+    keys, types, defaults, descriptions = obj.keys, obj.types, obj.defaults, obj.descriptions
+    for key in keys:
+        attr_docstrings.append(f'.. confval:: {key}\n'
+                               f'    :type: {types[key]}, defaults to {defaults[key]}\n\n'
+                               f'    {descriptions[key]}')
     attrs_docstring = '\n\n'.join(attr_docstrings)
     docstring = f"""
-This is a template for {cls.name}.
+This is a template for {cls.name}. 
 
-.. warning::
-    This is a template class just for documentation, do not use it directly.
+Examples
+--------
+>>> from abqpy.template import {cls.__name__}
+>>> template = {cls.__name__}(params='params.toml')
+>>> template.write('script.py')
+>>> template = {cls.__name__}()
+>>> template.write('script.py', param1=1, param2=2)
 
-.. note::
-    Details of the parameters requirements to render the template:
+.. admonition:: Details of required parameters
 
 {textwrap.indent(attrs_docstring, ' ' * 4)}
 
-    The source code of the template is::
+.. admonition:: User config file (A `toml <https://toml.io/>`_ or `json <https://www.json.org/>`_ file)
     
-{textwrap.indent(obj.source, ' ' * 8)}
+    .. code-block:: toml
+    
+{textwrap.indent(obj.params_source, ' ' * 8)}
+
+.. admonition:: The template source code
+
+    .. code-block:: python
+    
+{textwrap.indent(obj.template_source, ' ' * 8)}
+
+.. admonition:: The template config file  (A `toml <https://toml.io/>`_ or `json <https://www.json.org/>`_ file)
+    
+    .. code-block:: toml
+    
+{textwrap.indent(obj.config_source, ' ' * 8)}
 """
     cls.__doc__ = docstring
     return cls
 
 
 class _DocumentTemplate(ScriptTemplate):
+    #: The name of the template, the corresponding template filename must be `name.tmpl`, and the corresponding config
+    #: filename must be `name.toml`.
     name = 'template'
 
-    def __new__(cls):
+    def __new__(
+        cls,
+        params: Union[
+            Dict[str, Dict[str, Union[str, int, float, bool]]],
+            os.PathLike, str,
+        ] = None,
+    ):
         dirname = os.path.dirname(__file__)
-        source = os.path.join(dirname, '../../', 'templates', f'{cls.name}.tmp')
-        config = os.path.join(dirname, '../../', 'templates', f'{cls.name}.toml')
-        obj = super().__new__(cls, source, config)
-        obj.source = open(source, 'r', encoding='utf-8').read()
-        return obj
+        template = os.path.join(dirname, 'templates', f'{cls.name}.tmpl')
+        config = os.path.join(dirname, 'templates', f'{cls.name}.conf.toml')
+        if params is None:
+            params = os.path.join(dirname, 'templates', f'{cls.name}.toml')
+        return super().__new__(cls, template, config, params)
 
 
 @template_doc
-class _CompressionTemplate(_DocumentTemplate):
+class CompressionTemplate(_DocumentTemplate):
     name = 'compression'
 
 
@@ -171,10 +250,10 @@ def test_render():
     """Test the render function."""
     os.chdir(os.path.dirname(__file__))
     os.chdir('../../')
-    template = ScriptTemplate('templates/compression.tmp', 'templates/compression.toml')
+    template = ScriptTemplate('templates/compression.tmpl', 'templates/compression.toml')
     print(template.render(width=2, length=2, height=2))
 
 
 def test_decorator():
     """Test the decorator."""
-    print(_CompressionTemplate.__doc__)
+    print(CompressionTemplate.__doc__)
