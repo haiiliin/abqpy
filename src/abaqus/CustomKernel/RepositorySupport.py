96e9ebe6889c9c497b4c13703a2bdefecda4ee6a
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-09-30 00:20:07 +0800

    Fix import syntax error

--- src/abaqus/CustomKernel/RepositorySupport.py
+++ src/abaqus/CustomKernel/RepositorySupport.py
@@ -1,4 +1,4 @@
-from typing import Callable, typing.List, Union
+from typing import Callable, List, Union
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 from .CommandRegister import CommandRegister
