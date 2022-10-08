b48e46fde43cb5245db829d16c87a3d4c88aa6c0
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-10-09 00:04:29 +0800

    Resolve conflicts

--- src/abaqus/Interaction/InteractionModel.py
+++ src/abaqus/Interaction/InteractionModel.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from typing import Union, Optional
-=======
-from typing import Union, Optional, Sequence, Tuple
->>>>>>> 6ccc9236 (Use Sequence instead of Tuple in the typing annotations (#2566))
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 from .InteractionContactControlModel import InteractionContactControlModel
@@ -877,14 +873,7 @@ class InteractionModel(
         surfaceThicknessAssignments: Optional[SurfaceThicknessAssignment] = None, 
         surfaceOffsetAssignments: Optional[SurfaceOffsetAssignment] = None,
         surfaceFeatureAssignments: Optional[SurfaceFeatureAssignment] = None,
-<<<<<<< HEAD
         masterSlaveAssignments: Optional[MasterSlaveAssignment] = None,
-=======
-        surfaceBeamSmoothingAssignments: SurfaceBeamSmoothingAssignment = SurfaceBeamSmoothingAssignment(),
-        surfaceVertexCriteriaAssignments: SurfaceVertexCriteriaAssignment = SurfaceVertexCriteriaAssignment(),
-        slidingFormulationAssignments: Sequence[SlidingFormulationAssignment] = None,
-        mainSecondaryAssignments: Optional[MainSecondaryAssignment] = None, 
->>>>>>> 6ccc9236 (Use Sequence instead of Tuple in the typing annotations (#2566))
         initializationAssignments: Optional[InitializationAssignment] = None, 
         stabilizationAssignments: Optional[StabilizationAssignment] = None, 
         smoothingAssignments: Optional[SmoothingAssignment] = None, 
