b0c8c0fa661fa45caf4729daeba447d79cea05fa
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-10-09 00:12:37 +0800

    Add misssing imports

--- src/abaqus/Interaction/InteractionModel.py
+++ src/abaqus/Interaction/InteractionModel.py
@@ -1,4 +1,4 @@
-from typing import Union, Optional
+from typing import Union, Optional, Tuple
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 from .InteractionContactControlModel import InteractionContactControlModel
