aa1a619157adc9aa44d8ce73af6f6a6288d0303a
Diogo <rossi.diogo@gmail.com>
2022-10-15 03:13:52 -0300

    Improve types of `JobMdb` and `ModelJob` (#2783)
    * add `Optional` to arguments
    
    * add 2 new constants
    
    * improve types of `JobMdb` and `ModelJob`
    
    * Optimize imports
    
    Co-authored-by: Hailin-Wang <hailin.wang@connect.polyu.hk>
    (cherry picked from commit 6da034ee6a6b6d32a2d81118e099e8ab04d39cd4)
    
    # Conflicts:
    #	src/abaqus/Job/ModelJob.py

--- src/abaqus/PredefinedField/PorePressure.py
+++ src/abaqus/PredefinedField/PorePressure.py
@@ -1,11 +1,12 @@
 from typing import Union
+
 from typing_extensions import Literal
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 from .PredefinedField import PredefinedField
 from ..Region.Region import Region
-from ..UtilityAndView.abaqusConstants import abaqusConstants as C
 from ..UtilityAndView.abaqusConstants import Boolean, UNIFORM, CONSTANT_RATIO, MAGNITUDE
+from ..UtilityAndView.abaqusConstants import abaqusConstants as C
 
 
 @abaqus_class_doc
