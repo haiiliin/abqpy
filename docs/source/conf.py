592393eba25076e539253402dacd6ba8debc68c8
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-10-23 22:18:27 +0800

    [docs]: Use short version in docs (#3110)
    (cherry picked from commit 08f87f30ced9fcb1c7b36c723cd17f30629edacf)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,11 +32,11 @@ author = 'WANG Hailin'
 # The full version, including alpha/beta/rc tags
 try:
     import abqpy
-    release = version = abqpy.__semver__
+    release = version = abqpy.__semver__[:4]
 except (ImportError, AttributeError):
     import warnings
     warnings.warn('abqpy is not installed, using 2023.0.0')
-    release = version = '2023.0.0'
+    release = version = '2023.0.0'[:4]
 sys.path.insert(0, os.path.abspath('../../src'))
 
 # For multiple languages
