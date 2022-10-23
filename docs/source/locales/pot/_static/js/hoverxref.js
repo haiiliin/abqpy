5eed604be41285c25feb0b4f29164a124e998591
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-10-23 08:47:27 +0800

    [translation]: Update translations (2022/10/23) (#3001)
    Update translations by GitHub Action https://github.com/haiiliin/abqpy/actions/runs/3305239248
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/source/locales/pot/_static/js/hoverxref.js
+++ docs/source/locales/pot/_static/js/hoverxref.js
@@ -82,7 +82,7 @@ function reLoadSphinxTabs() {
 function getEmbedURL(url) {
     var params = {
         'doctool': 'sphinx',
-        'doctoolversion': '5.2.3',
+        'doctoolversion': '5.3.0',
         'url': url,
     }
     console.debug('Data: ' + JSON.stringify(params));
