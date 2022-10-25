1cfe5f4fd891aaaa10902d73b36f43b004f64c65
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-10-25 19:57:38 +0800

    Resolve conflicts

--- src/displayGroupMdbToolset.py
+++ src/displayGroupMdbToolset.py
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-from abaqus.DisplayGroup.Leaf import Leaf
-from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup
-from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels
-from abaqus.DisplayGroup.LeafFromElementSets import LeafFromElementSets
-from abaqus.DisplayGroup.LeafFromElementVarRange import LeafFromElementVarRange
-from abaqus.DisplayGroup.LeafFromModelElemLabels import LeafFromModelElemLabels
-from abaqus.DisplayGroup.LeafFromModelNodeLabels import LeafFromModelNodeLabels
-from abaqus.DisplayGroup.LeafFromNodeLabels import LeafFromNodeLabels
-from abaqus.DisplayGroup.LeafFromNodeSets import LeafFromNodeSets
-from abaqus.DisplayGroup.LeafFromNodeVarRange import LeafFromNodeVarRange
-from abaqus.DisplayGroup.LeafFromOdbEdgePick import LeafFromOdbEdgePick
-from abaqus.DisplayGroup.LeafFromOdbElementLayups import LeafFromOdbElementLayups
-from abaqus.DisplayGroup.LeafFromOdbElementMaterials import LeafFromOdbElementMaterials
-from abaqus.DisplayGroup.LeafFromOdbElementPick import LeafFromOdbElementPick
-from abaqus.DisplayGroup.LeafFromOdbElementPlies import LeafFromOdbElementPlies
-from abaqus.DisplayGroup.LeafFromOdbElementSections import LeafFromOdbElementSections
-from abaqus.DisplayGroup.LeafFromOdbElementTypes import LeafFromOdbElementTypes
-from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick
-from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance
-from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets
-from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange
-=======
 from abaqus.DisplayGroup.Leaf import Leaf  # noqa # pylint: disable=unused-import
-from abaqus.DisplayGroup.LeafFromConstraintNames import LeafFromConstraintNames  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromElementSets import LeafFromElementSets  # noqa # pylint: disable=unused-import
@@ -43,4 +19,3 @@ from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick  # noqa 
 from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange  # noqa # pylint: disable=unused-import
->>>>>>> 6b099ab1 (Add math functions and ignore unused imports (#3290))
