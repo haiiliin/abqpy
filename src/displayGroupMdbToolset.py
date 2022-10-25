80d5b2e1403fe465c805e7cfe51e37119b5e568a
Hailin Wang <47051427+haiiliin@users.noreply.github.com>
2022-10-25 19:47:08 +0800

    Add math functions and ignore unused imports (#3290)
    * Add math functions to abaqus and odbAccess modules
    
    * Disable unused imports warnings
    
    (cherry picked from commit 6b099ab1c2c39c9d81e6558dc59a81417977fe7b)
    
    # Conflicts:
    #	src/displayGroupMdbToolset.py

--- src/displayGroupMdbToolset.py
+++ src/displayGroupMdbToolset.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from abaqus.DisplayGroup.Leaf import Leaf
 from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup
 from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels
@@ -19,3 +20,27 @@ from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick
 from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance
 from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets
 from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange
+=======
+from abaqus.DisplayGroup.Leaf import Leaf  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromConstraintNames import LeafFromConstraintNames  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementSets import LeafFromElementSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementVarRange import LeafFromElementVarRange  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromModelElemLabels import LeafFromModelElemLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromModelNodeLabels import LeafFromModelNodeLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeLabels import LeafFromNodeLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeSets import LeafFromNodeSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeVarRange import LeafFromNodeVarRange  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbEdgePick import LeafFromOdbEdgePick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementLayups import LeafFromOdbElementLayups  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementMaterials import LeafFromOdbElementMaterials  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementPick import LeafFromOdbElementPick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementPlies import LeafFromOdbElementPlies  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementSections import LeafFromOdbElementSections  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementTypes import LeafFromOdbElementTypes  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange  # noqa # pylint: disable=unused-import
+>>>>>>> 6b099ab1 (Add math functions and ignore unused imports (#3290))
