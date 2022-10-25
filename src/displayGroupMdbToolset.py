81c9e2048346d98f175c91167dc4ad092b37a6e0
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-10-25 19:22:49 +0800

    Disable unused imports warnings

--- src/displayGroupMdbToolset.py
+++ src/displayGroupMdbToolset.py
@@ -1,22 +1,22 @@
-from abaqus.DisplayGroup.Leaf import Leaf
-from abaqus.DisplayGroup.LeafFromConstraintNames import LeafFromConstraintNames
-from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup
-from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels
-from abaqus.DisplayGroup.LeafFromElementSets import LeafFromElementSets
-from abaqus.DisplayGroup.LeafFromElementVarRange import LeafFromElementVarRange
-from abaqus.DisplayGroup.LeafFromModelElemLabels import LeafFromModelElemLabels
-from abaqus.DisplayGroup.LeafFromModelNodeLabels import LeafFromModelNodeLabels
-from abaqus.DisplayGroup.LeafFromNodeLabels import LeafFromNodeLabels
-from abaqus.DisplayGroup.LeafFromNodeSets import LeafFromNodeSets
-from abaqus.DisplayGroup.LeafFromNodeVarRange import LeafFromNodeVarRange
-from abaqus.DisplayGroup.LeafFromOdbEdgePick import LeafFromOdbEdgePick
-from abaqus.DisplayGroup.LeafFromOdbElementLayups import LeafFromOdbElementLayups
-from abaqus.DisplayGroup.LeafFromOdbElementMaterials import LeafFromOdbElementMaterials
-from abaqus.DisplayGroup.LeafFromOdbElementPick import LeafFromOdbElementPick
-from abaqus.DisplayGroup.LeafFromOdbElementPlies import LeafFromOdbElementPlies
-from abaqus.DisplayGroup.LeafFromOdbElementSections import LeafFromOdbElementSections
-from abaqus.DisplayGroup.LeafFromOdbElementTypes import LeafFromOdbElementTypes
-from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick
-from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance
-from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets
-from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange
+from abaqus.DisplayGroup.Leaf import Leaf  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromConstraintNames import LeafFromConstraintNames  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementLabels import LeafFromElementLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementSets import LeafFromElementSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromElementVarRange import LeafFromElementVarRange  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromModelElemLabels import LeafFromModelElemLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromModelNodeLabels import LeafFromModelNodeLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeLabels import LeafFromNodeLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeSets import LeafFromNodeSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromNodeVarRange import LeafFromNodeVarRange  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbEdgePick import LeafFromOdbEdgePick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementLayups import LeafFromOdbElementLayups  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementMaterials import LeafFromOdbElementMaterials  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementPick import LeafFromOdbElementPick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementPlies import LeafFromOdbElementPlies  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementSections import LeafFromOdbElementSections  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbElementTypes import LeafFromOdbElementTypes  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromOdbNodePick import LeafFromOdbNodePick  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromPartInstance import LeafFromPartInstance  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromSurfaceSets import LeafFromSurfaceSets  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromSurfaceVarRange import LeafFromSurfaceVarRange  # noqa # pylint: disable=unused-import
--- src/odbAccess.py
+++ src/odbAccess.py
@@ -4,7 +4,7 @@ import abqpy.abaqus
 
 abqpy.abaqus.run(cae=False)
 
-from abaqus.Odb.OdbCommands import *
+from abaqus.Odb.OdbCommands import *  # noqa # pylint: disable=unused-import
 from abaqus.UtilityAndView.BackwardCompatibility import BackwardCompatibility
 
 backwardCompatibility = BackwardCompatibility()
