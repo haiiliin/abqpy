3de1810d6b42b0bdc8ea654895092ad53f8cd343
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-11-21 15:41:31 +0800

    Use typing_extensions.Literal

--- src/abaqus/Optimization/BeadFilter.py
+++ src/abaqus/Optimization/BeadFilter.py
@@ -1,6 +1,7 @@
-from typing import Literal, Union
+from typing import Union
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
+from typing_extensions import Literal
 
 from .GeometricRestriction import GeometricRestriction
 from ..Region.Region import Region
