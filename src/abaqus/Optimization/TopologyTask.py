d9fc8d8022b19ac63d9a9f62db265b79eeb1564b
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-04-23 20:32:46 +0800

    [refactor] refactor the abaqus.Material.Others package (#4130)
    * [refactor] refactor the abaqus.Material.Others package
    
    * Replace × to x
    
    * Fix tests
    
    * Update README-zh-cn.md
    
    (cherry picked from commit 39cbd8359d2140c43c46226534f997e5bb5ddb48)
    
    # Conflicts:
    #	README-zh-cn.md
    #	src/abaqus/Material/MaterialBase.py

--- src/abaqus/Optimization/TopologyTask.py
+++ src/abaqus/Optimization/TopologyTask.py
@@ -77,7 +77,7 @@ class TopologyTask(OptimizationTask):
     densityUpdateStrategy: SymbolicConstant = NORMAL
 
     #: A Float specifying the stop criteria based upon the change in element densities. The
-    #: default value is 0.5×10⁻².
+    #: default value is 0.5x10⁻².
     elementDensityDeltaStopCriteria: float = 0
 
     #: None or a Float specifying the mesh filter radius for mesh independence and minimum
@@ -270,7 +270,7 @@ class TopologyTask(OptimizationTask):
             The default value is NORMAL.
         elementDensityDeltaStopCriteria
             A Float specifying the stop criteria based upon the change in element densities. The
-            default value is 0.5×10⁻².
+            default value is 0.5x10⁻².
         filterRadius
             None or a Float specifying the mesh filter radius for mesh independence and minimum
             size. The default value is None.
@@ -434,7 +434,7 @@ class TopologyTask(OptimizationTask):
             The default value is NORMAL.
         elementDensityDeltaStopCriteria
             A Float specifying the stop criteria based upon the change in element densities. The
-            default value is 0.5×10⁻².
+            default value is 0.5x10⁻².
         filterRadius
             None or a Float specifying the mesh filter radius for mesh independence and minimum
             size. The default value is None.
