9553727e55bf00e66826da062b289b23066d045b
Diogo <rossi.diogo@gmail.com>
2023-04-23 21:45:29 -0300

    [typing] add `abaqusConstants` to `odbAccess` module (#4146)
    (cherry picked from commit d26d6db29c8b3a1b44c2a25a1bfd24e33a4180cd)

--- src/odbAccess.py
+++ src/odbAccess.py
@@ -6,5 +6,6 @@ abqpy.abaqus.run(cae=False)
 
 from abaqus.Odb.OdbCommands import *  # noqa # pylint: disable=unused-import
 from abaqus.UtilityAndView.BackwardCompatibility import BackwardCompatibility
+from abaqusConstants import *  # noqa # pylint: disable=unused-import
 
 backwardCompatibility = BackwardCompatibility()
