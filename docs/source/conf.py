4bbf1d6408c15cbc0f5a65a510b8085c474d577a
Hailin-Wang <hailin.wang@connect.polyu.hk>
2023-04-25 21:40:42 +0800

    [bugfix] Ignore _autoapi_templates/index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -192,7 +192,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["locales/README.md"]
+exclude_patterns = ["locales/README.md", "_autoapi_templates"]
 
 # -- Options for HTML output -------------------------------------------------
 
