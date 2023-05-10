a4fe919c5d12de5a463a7074ad5150db8393fb26
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-05-10 14:04:37 +0800

    [workflow] format po files (#4291)
    * [workflow] format po files
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit b18cbe8e4f610b9dec6564f14444084188b038a7)
    
    # Conflicts:
    #	docs/source/conf.py

--- docs/source/_ext/classdocumenter.py
+++ docs/source/_ext/classdocumenter.py
@@ -11,23 +11,23 @@ class AbqpyClassDocumenter(ClassDocumenter):
         _, members = self.get_object_members(want_all)
         if members:
             sourcename = self.get_sourcename()
-            self.add_line('', sourcename)
-            self.add_line('.. rubric:: Member Details:', sourcename)
-            self.add_line('', sourcename)
-            self.add_line('.. toggle::', sourcename)
-            self.add_line('', sourcename)
-            self.add_line('    .. placeholder for empty members', sourcename)
-            self.add_line('', sourcename)
-            self.indent += ' ' * 4
+            self.add_line("", sourcename)
+            self.add_line(".. rubric:: Member Details:", sourcename)
+            self.add_line("", sourcename)
+            self.add_line(".. toggle::", sourcename)
+            self.add_line("", sourcename)
+            self.add_line("    .. placeholder for empty members", sourcename)
+            self.add_line("", sourcename)
+            self.indent += " " * 4
         super().document_members(all_members)
 
 
 def setup(app: Sphinx):
-    app.setup_extension('sphinx.ext.autodoc')  # Require autodoc extension
-    app.setup_extension('sphinx_togglebutton')  # Require sphinx-togglebutton extension
+    app.setup_extension("sphinx.ext.autodoc")  # Require autodoc extension
+    app.setup_extension("sphinx_togglebutton")  # Require sphinx-togglebutton extension
     app.add_autodocumenter(AbqpyClassDocumenter, override=True)
     return {
-        'version': '0.0.1',
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "version": "0.0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
--- docs/source/_ext/version.py
+++ docs/source/_ext/version.py
@@ -16,11 +16,12 @@ class VersionRole(SphinxRole):
         :version-patch:`numpy`
         :version:`prefix text |numpy| suffix text`
     """
-    type = 'version'
+
+    type = "version"
 
     def run(self):
-        prefix, suffix, package = '', '', ''
-        elements = self.text.split('|', 3)
+        prefix, suffix, package = "", "", ""
+        elements = self.text.split("|", 3)
         if len(elements) == 1:
             package = elements[0]
         elif len(elements) == 2:
@@ -28,32 +29,37 @@ class VersionRole(SphinxRole):
         elif len(elements) == 3:
             prefix, package, suffix = elements
         else:
-            raise ValueError('Invalid version role syntax')
+            raise ValueError("Invalid version role syntax")
 
-        major, minor, patch = metadata.version(package).split('.')[:3]
-        version = (major if self.type == 'major' else
-                   minor if self.type == 'minor' else
-                   patch if self.type == 'patch' else
-                   f'{major}.{minor}.{patch}')
-        text = f'{prefix}{version}{suffix}'
+        major, minor, patch = metadata.version(package).split(".")[:3]
+        version = (
+            major
+            if self.type == "major"
+            else minor
+            if self.type == "minor"
+            else patch
+            if self.type == "patch"
+            else f"{major}.{minor}.{patch}"
+        )
+        text = f"{prefix}{version}{suffix}"
         return [nodes.Text(text, text)], []
 
 
 class MajorVersionRole(VersionRole):
-    type = 'major'
+    type = "major"
 
 
 class MinorVersionRole(VersionRole):
-    type = 'minor'
+    type = "minor"
 
 
 class PatchVersionRole(VersionRole):
-    type = 'patch'
+    type = "patch"
 
 
 def setup(app: Sphinx):
-    app.add_role('version', VersionRole())
-    app.add_role('version-major', MajorVersionRole())
-    app.add_role('version-minor', MinorVersionRole())
-    app.add_role('version-patch', PatchVersionRole())
-    return {'version': '0.0.1', 'parallel_read_safe': True}
+    app.add_role("version", VersionRole())
+    app.add_role("version-major", MajorVersionRole())
+    app.add_role("version-minor", MinorVersionRole())
+    app.add_role("version-patch", PatchVersionRole())
+    return {"version": "0.0.1", "parallel_read_safe": True}
