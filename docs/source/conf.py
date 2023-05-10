872c974d10bf3fd0f1a41de56f35e65627d86e66
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-05-10 14:10:20 +0800

    Resolve conflicts

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,18 +36,11 @@ try:
     release, version = abqpy.__version__, abqpy.__semver__.split(".")[0]
 except (ImportError, AttributeError):
     import warnings
-<<<<<<< HEAD
-    warnings.warn('abqpy is not installed, using 2023.0.0')
-    release, version = '2016.0.0', '2016'
-sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('./_ext'))
-=======
 
-    warnings.warn("abqpy is not installed, using 2023.0.0")
-    release, version = "2023.0.0", "2023"
+    warnings.warn("abqpy is not installed, using 2016.0.0")
+    release, version = "2016.0.0", "2016"
 sys.path.insert(0, os.path.abspath("../../src"))
 sys.path.insert(0, os.path.abspath("./_ext"))
->>>>>>> b18cbe8e ([workflow] format po files (#4291))
 
 # For multiple languages
 locale_dirs = ["locales/"]  # path is example but recommended.
