f67ffa46c82952d9b6fcc1b8eae8828760981012
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-05-10 14:04:37 +0800

    [workflow] format po files (#4291)
    * [workflow] format po files
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit b18cbe8e4f610b9dec6564f14444084188b038a7)
    
    # Conflicts:
    #	docs/source/conf.py

--- src/displayGroupOdbToolset.py
+++ src/displayGroupOdbToolset.py
@@ -3,12 +3,20 @@ from abaqus.DisplayGroup.LeafFromDatums import LeafFromDatums  # noqa # pylint: 
 from abaqus.DisplayGroup.LeafFromDisplayGroup import LeafFromDisplayGroup  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromGeometry import LeafFromGeometry  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromInstance import LeafFromInstance  # noqa # pylint: disable=unused-import
-from abaqus.DisplayGroup.LeafFromInstanceElementLabels import LeafFromInstanceElementLabels  # noqa # pylint: disable=unused-import
-from abaqus.DisplayGroup.LeafFromInstanceNodeLabels import LeafFromInstanceNodeLabels  # noqa # pylint: disable=unused-import
-from abaqus.DisplayGroup.LeafFromMeshElementLabels import LeafFromMeshElementLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromInstanceElementLabels import (
+    LeafFromInstanceElementLabels,
+)  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromInstanceNodeLabels import (
+    LeafFromInstanceNodeLabels,
+)  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromMeshElementLabels import (
+    LeafFromMeshElementLabels,
+)  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromMeshNodeLabels import LeafFromMeshNodeLabels  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromMeshSurfaceSets import LeafFromMeshSurfaceSets  # noqa # pylint: disable=unused-import
-from abaqus.DisplayGroup.LeafFromPartElementLabels import LeafFromPartElementLabels  # noqa # pylint: disable=unused-import
+from abaqus.DisplayGroup.LeafFromPartElementLabels import (
+    LeafFromPartElementLabels,
+)  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromPartNodeLabels import LeafFromPartNodeLabels  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromReferencePoint import LeafFromReferencePoint  # noqa # pylint: disable=unused-import
 from abaqus.DisplayGroup.LeafFromSets import LeafFromSets  # noqa # pylint: disable=unused-import
