70a4141fc5077a8651a634b9cd7acf04868298d8
haiiliin <hailin.wang@connect.polyu.hk>
2023-05-13 21:34:20 +0800

    Update rtd.sh

--- scripts/rtd.sh
+++ scripts/rtd.sh
@@ -4,17 +4,12 @@
 set -ex
 cd "$(dirname "$0")"
 
-# Activate the current versions and hide the previous ones
+# Activate and build the current versions, hide the previous ones
 for MAJOR in $(seq 2016 2023)
 do
     python rtd.py version update --project=$PROJECT --version=v$MAJOR.$CURRENT_MINOR_PATCH --active --nohidden
+    python rtd.py version build  --project=$PROJECT --version=v$MAJOR.$CURRENT_MINOR_PATCH
     python rtd.py version update --project=$PROJECT --version=v$MAJOR.$PREVIOUS_MINOR_PATCH --hidden
-done
-
-# Build the versions
-for MAJOR in $(seq 2016 2023)
-do
-    python rtd.py version build --project=$PROJECT --version=v$MAJOR.$CURRENT_MINOR_PATCH
 done
 
 # Update the redirects to make `20**` point to the latest versions
