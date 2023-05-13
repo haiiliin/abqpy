c7c6f87244047028a8837b442b7bf25b95918884
haiiliin <hailin.wang@connect.polyu.hk>
2023-05-13 16:11:48 +0800

    Delete test.sh

--- scripts/rtd.sh
+++ scripts/rtd.sh
@@ -6,6 +6,5 @@ cd "$(dirname "$0")"
 
 for MAJOR in $(seq 2016 2023)
 do
-    echo "$MAJOR"
     python rtd.py version update --version=v$MAJOR.$MINOR_PATCH --project=$PROJECT $ARGS
 done
--- test.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 2016 2023)
-do
-    echo "$i"
-done
