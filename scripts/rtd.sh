fb426430b5e9aa31a5c771780da00e8034d41c0e
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-05-13 21:36:59 +0800

    [skip ci][workflow] Build rtd version on releases (#4447)
    * [workflow] Build rtd version on releases
    
    * Update rtd.sh
    
    * Update rtd.sh
    
    (cherry picked from commit c8dd409cf2517c8ab84c0db46ba42656ee4a99c3)

--- scripts/rtd.sh
+++ scripts/rtd.sh
@@ -4,11 +4,13 @@
 set -ex
 cd "$(dirname "$0")"
 
-# Activate the current versions and hide the previous ones
+# Activate and build the current versions, hide the previous ones
 for MAJOR in $(seq 2016 2023)
 do
-    python rtd.py version update --project=$PROJECT --version=v$MAJOR.$MINOR_PATCH $ARGS
+    python rtd.py version update --project=$PROJECT --version=v$MAJOR.$CURRENT_MINOR_PATCH --active --nohidden
+    python rtd.py version build  --project=$PROJECT --version=v$MAJOR.$CURRENT_MINOR_PATCH
+    python rtd.py version update --project=$PROJECT --version=v$MAJOR.$PREVIOUS_MINOR_PATCH --hidden
 done
 
 # Update the redirects to make `20**` point to the latest versions
-python rtd.py redirect update --project=$PROJECT --minor_patch="$MINOR_PATCH"
+python rtd.py redirect update --project=$PROJECT --minor_patch="$CURRENT_MINOR_PATCH"
