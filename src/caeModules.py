0a4abd449656638c72e6f3a50483275de05bbbc4
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-07-11 15:43:57 +0800

    [bugfix] Import modules in caeModules (#4682)

--- src/caeModules.py
+++ src/caeModules.py
@@ -1,1 +1,24 @@
-import mesh  # noqa # pylint: disable=unused-import
+from os import environ as _environ
+
+import abaqus
+import assembly
+import connector
+import connectorBehavior
+import displayGroupMdbToolset as dgm
+import displayGroupOdbToolset as dgo
+import interaction
+import job
+import load
+import material
+import mesh
+import optimization
+import part
+import regionToolset
+import section
+import sketch
+import step
+import visualization
+import xyPlot
+
+# if 'HKS_WITH_STUB' in _environ:
+#     import stub
