3d6b4bec0ee5ee2f6cf81cb0223f4b7ef15cdaa1
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-07-12 20:32:49 +0800

    [typing] Remove noqas (#4715)
    * [typing] Remove noqas
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit f9ff27da4b05f6126c549a36d314ab65f1c21208)
    
    # Conflicts:
    #	src/displayGroupMdbToolset.py

--- src/optimization.py
+++ src/optimization.py
@@ -1,17 +1,18 @@
-from regionToolset import Region  # noqa # pylint: disable=unused-import
-from sketch import Sketch  # noqa # pylint: disable=unused-import
-from sketch import SketchTransform  # noqa # pylint: disable=unused-import
-from xyPlot import AreaStyle  # noqa # pylint: disable=unused-import
-from xyPlot import LineStyle  # noqa # pylint: disable=unused-import
-from xyPlot import QuantityType  # noqa # pylint: disable=unused-import
-from xyPlot import SymbolStyle  # noqa # pylint: disable=unused-import
-from xyPlot import TextStyle  # noqa # pylint: disable=unused-import
-from xyPlot import XYData  # noqa # pylint: disable=unused-import
-from xyPlot import XYDataFromFile  # noqa # pylint: disable=unused-import
-from xyPlot import XYDataFromFreeBody  # noqa # pylint: disable=unused-import
-from xyPlot import XYDataFromHistory  # noqa # pylint: disable=unused-import
-from xyPlot import XYDataFromPath  # noqa # pylint: disable=unused-import
-from xyPlot import XYDataFromShellThickness  # noqa # pylint: disable=unused-import
-from xyPlot import xyDataListFromField  # noqa # pylint: disable=unused-import
+from regionToolset import Region
+from sketch import Sketch, SketchTransform
+from xyPlot import (
+    AreaStyle,
+    LineStyle,
+    QuantityType,
+    SymbolStyle,
+    TextStyle,
+    XYData,
+    XYDataFromFile,
+    XYDataFromFreeBody,
+    XYDataFromHistory,
+    XYDataFromPath,
+    XYDataFromShellThickness,
+    xyDataListFromField,
+)
 
 # inspected from Abaqus cli
