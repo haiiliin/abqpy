ec05fa21d9c5191599a79f8ba42cd6b5182f93fb
haiiliin <hailin.wang@connect.polyu.hk>
2023-07-20 12:39:46 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -225,6 +225,7 @@ if html_theme == "sphinx_book_theme":
     html_theme_options = {
         "repository_url": "https://github.com/haiiliin/abqpy",
         "use_source_button": True,
+        "path_to_docs": "docs/source",
         "repository_branch": branch,
         "use_edit_page_button": True,
         "use_repository_button": True,
