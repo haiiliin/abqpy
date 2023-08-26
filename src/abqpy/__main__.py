710c3cddf5c54df077358599639fbf871933e180
haiiliin <hailin.wang@connect.polyu.hk>
2023-08-26 19:40:26 +0800

    [skip ci] Temporarily suppress the traceback to be shown

--- src/abqpy/__main__.py
+++ src/abqpy/__main__.py
@@ -1,3 +1,5 @@
+import sys
+
 import fire
 
 from .cli import AbqpyCLI
@@ -7,6 +9,7 @@ def main():
     """The abqpy command line interface."""
     # Print to stdout, a workaround from https://github.com/google/python-fire/issues/188#issuecomment-1528976874
     fire.core.Display = lambda lines, out: out.write("\n".join(lines) + "\n")
+    sys.tracebacklimit = 0
     fire.Fire(AbqpyCLI())
 
 
