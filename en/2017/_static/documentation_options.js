d909e928dcfc6be8382dcfa0194007017af38154
haiiliin <haiiliin@users.noreply.github.com>
2023-09-07 13:13:25 +0000

    deploy: e6552a8726ef175f6679613ee885cb55ba8e6a31

--- en/2017/_static/documentation_options.js
+++ en/2017/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2017.5.7.dev2+g1382a4455',
+    VERSION: '2017.5.7',
     LANGUAGE: 'en/html',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
