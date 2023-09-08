74f217833e526afd16b2570cef40ee17df92e289
haiiliin <haiiliin@users.noreply.github.com>
2023-09-08 02:26:55 +0000

    deploy: b8bda0d617e6817065b197d5720ecfb25bc9ab80

--- zh_CN/2019/_static/documentation_options.js
+++ zh_CN/2019/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2019.5.7.dev8+g4249b09ab',
+    VERSION: '2019.5.7',
     LANGUAGE: 'zh-CN/html',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
