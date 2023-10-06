061c95118d42a011228b1bc7faf77fb32ffe55e2
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-10-06 23:10:11 +0800

    [bugfix] Fix odb session imports in #5263

--- src/abaqus/Odb/OdbSession.py
+++ src/abaqus/Odb/OdbSession.py
@@ -3,6 +3,7 @@ from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 from ..Session.SessionBase import SessionBase
 from .Odb import Odb
 from .ScratchOdb import ScratchOdb
+from ..UtilityAndView.abaqusConstants import Boolean, OFF
 
 
 @abaqus_class_doc
