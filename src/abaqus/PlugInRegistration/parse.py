dcbfa507a0f8bf6b95dba9bbd6caa7c752235083
Hailin Wang <hailin.wang@connect.polyu.hk>
2023-10-06 13:20:26 +0800

    Update parse.py

--- src/abaqus/PlugInRegistration/parse.py
+++ src/abaqus/PlugInRegistration/parse.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
+import re
 from pathlib import Path
 
-import mistletoe
-from mistletoe.block_token import Paragraph, Heading, BlockToken
 from pydantic import BaseModel
 
 
@@ -32,19 +31,24 @@ class Class(BaseModel):
 
 for md in Path(".").glob("*.md"):
     with md.open("r", encoding="utf-8") as f:
-        doc = mistletoe.Document(f.readlines())
+        lines = f.readlines()
 
-    data = {}
     sections, current_section = [], []
-    for child in doc.children:  # type: BlockToken
-        if not isinstance(child, Heading):
-            current_section.append(child)
-        else:
+    for line in lines:
+        line = re.sub(r"!\[.*]\(.*\)", "", line)
+        if not line.strip():
+            continue
+        if line.startswith("#"):
             sections.append(current_section)
-            current_section = [child]
+            current_section = [line.lstrip("#").strip()]
+        else:
+            current_section.append(line.strip())
+    sections.append(current_section)
 
-    data["description"] = sections[0][0].children[0].content
-    heading: Heading
-    paragraphs: list[Paragraph]
+    data = {"description": sections[0][0]}
+    heading: list[str]
+    paragraphs: list[list[str]]
     for heading, *paragraphs in sections[1:]:
         pass
+
+    break
