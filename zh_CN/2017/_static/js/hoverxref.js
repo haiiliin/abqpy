e3c26ba337b77559415bfb308e6d876cc80ea4f0
haiiliin <haiiliin@users.noreply.github.com>
2024-03-19 02:14:41 +0000

    deploy: f95342640ffc42f7abca2539a56b32a6c1820265

--- zh_CN/2017/_static/js/hoverxref.js
+++ zh_CN/2017/_static/js/hoverxref.js
@@ -82,7 +82,7 @@ function reLoadSphinxTabs() {
 function getEmbedURL(url) {
     var params = {
         'doctool': 'sphinx',
-        'doctoolversion': '6.2.1',
+        'doctoolversion': '7.2.6',
         'url': url,
     }
     console.debug('Data: ' + JSON.stringify(params));
