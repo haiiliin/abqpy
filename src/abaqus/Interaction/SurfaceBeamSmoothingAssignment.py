0914e7e1415cdc874688218b11d3dba6411fb413
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-08-28 15:35:05 +0800

    [typing] Fix typing errors for argument `assignments` in `appendInStep`

--- src/abaqus/Interaction/SurfaceBeamSmoothingAssignment.py
+++ src/abaqus/Interaction/SurfaceBeamSmoothingAssignment.py
@@ -1,5 +1,7 @@
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 
+from ..Region.Region import Region
+
 
 @abaqus_class_doc
 class SurfaceBeamSmoothingAssignment:
@@ -41,7 +43,7 @@ class SurfaceBeamSmoothingAssignment:
         ...
 
     @abaqus_method_doc
-    def appendInStep(self, stepName: str, assignments: tuple):
+    def appendInStep(self, stepName: str, assignments: tuple[tuple[Region, float], ...]):
         """This method allows addition of surface beam smoothing assignments to new surfaces in a given step.
 
         Parameters
@@ -51,8 +53,10 @@ class SurfaceBeamSmoothingAssignment:
             are to be defined.
         assignments
             A sequence of tuples specifying the surface beam smoothing assignments. Each tuple
-            contains two entries:A region object specifying the surface to which the smoothing is
-            assigned.A Float specifying the surface smoothing value to be used for the surface.
+            contains two entries:
+
+            - A region object specifying the surface to which the smoothing is assigned.
+            - A Float specifying the surface smoothing value to be used for the surface.
         """
         ...
 
