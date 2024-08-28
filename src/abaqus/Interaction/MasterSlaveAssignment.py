2211099fd58d233585db4f237c5c06d3bb5cdbcf
Hailin Wang <hailin.wang@connect.polyu.hk>
2024-08-28 16:28:42 +0800

    [typing] Fix typing errors for argument `assignments` in `appendInStep` (#5734)
    (cherry picked from commit 4e9388edd251ac4ddf07631835a53577b7f00303)
    
    # Conflicts:
    #	src/abaqus/Interaction/MasterSlaveAssignment.py
    #	src/abaqus/Interaction/SlidingFormulationAssignment.py
    #	src/abaqus/Interaction/SurfaceBeamSmoothingAssignment.py
    #	src/abaqus/Interaction/SurfaceCrushTriggerAssignment.py
    #	src/abaqus/Interaction/SurfaceFeatureAssignment.py
    #	src/abaqus/Interaction/SurfaceFrictionAssignment.py
    #	src/abaqus/Interaction/SurfaceVertexCriteriaAssignment.py

--- src/abaqus/Interaction/MasterSlaveAssignment.py
+++ src/abaqus/Interaction/MasterSlaveAssignment.py
@@ -4,6 +4,7 @@ from typing_extensions import Literal
 
 from abqpy.decorators import abaqus_class_doc, abaqus_method_doc
 
+from ..Region.Region import Region
 from ..UtilityAndView.abaqusConstants import abaqusConstants as C
 
 
@@ -44,8 +45,17 @@ class MasterSlaveAssignment:
         ...
 
     @abaqus_method_doc
+<<<<<<< HEAD:src/abaqus/Interaction/MasterSlaveAssignment.py
     def appendInStep(self, stepName: str, assignments: Literal[C.BALANCED, C.SECONDARY, C.MAIN, C.GLOBAL]):
         """This method allows addition of master-slave assignments to new surface pairs in a given step.
+=======
+    def appendInStep(
+        self,
+        stepName: str,
+        assignments: tuple[tuple[Region | Literal[C.GLOBAL], Literal[C.BALANCED, C.SECONDARY, C.MAIN, C.GLOBAL]], ...],
+    ):
+        """This method allows addition of main-secondary assignments to new surface pairs in a given step.
+>>>>>>> 4e9388ed ([typing] Fix typing errors for argument `assignments` in `appendInStep` (#5734)):src/abaqus/Interaction/MainSecondaryAssignment.py
 
         Parameters
         ----------
