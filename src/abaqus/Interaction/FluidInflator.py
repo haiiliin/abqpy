25a7f616530ed4015d3225e3b4991f06f05833b9
Hailin Wang <wanghailin@tongji.edu.cn>
2024-12-07 22:35:18 +0800

    Update FluidInflator.py

--- src/abaqus/Interaction/FluidInflator.py
+++ src/abaqus/Interaction/FluidInflator.py
@@ -31,8 +31,6 @@ class FluidInflator(Interaction):
         createStepName: str,
         cavity: str,
         interactionProperty: str,
-        inflationTimeAmplitude: str = "",
-        massFlowAmplitude: str = "",
     ):
         """This method creates a FluidInflator object.
 
@@ -51,12 +49,6 @@ class FluidInflator(Interaction):
             A String specifying the first FluidCavity object associated with this interaction.
         interactionProperty
             A String specifying the FluidInflatorProperty object associated with this interaction.
-        inflationTimeAmplitude
-            A string specifying the name of the amplitude curve defining a mapping between the
-            inflation time and the actual time.
-        massFlowAmplitude
-            A string specifying the name of the amplitude curve by which to modify the mass flow
-            rate.
 
         Returns
         -------
@@ -65,16 +57,6 @@ class FluidInflator(Interaction):
         super().__init__()
 
     @abaqus_method_doc
-    def setValues(self, inflationTimeAmplitude: str = "", massFlowAmplitude: str = ""):
-        """This method modifies the FluidInflator object.
-
-        Parameters
-        ----------
-        inflationTimeAmplitude
-            A string specifying the name of the amplitude curve defining a mapping between the
-            inflation time and the actual time.
-        massFlowAmplitude
-            A string specifying the name of the amplitude curve by which to modify the mass flow
-            rate.
-        """
+    def setValues(self):
+        """This method modifies the FluidInflator object."""
         ...
