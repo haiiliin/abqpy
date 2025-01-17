bae141ef56c8a66724c81de21efb48e4776bb0fc
Diogo <rossi.diogo@gmail.com>
2025-01-17 03:35:28 -0300

    [feature] Add some missing members (#5903)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hailin Wang <wanghailin@tongji.edu.cn>
    Co-authored-by: Hailin Wang <hailin.wang@connect.polyu.hk>
    (cherry picked from commit 0175c7325de91308d47053d7b8ef984d1b921a06)

--- src/abaqus/__init__.py
+++ src/abaqus/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import auto_all
 
-from abqpy import run  # noqa
+from abqpy import run, version_info  # noqa
 
 run(cae=True)
 auto_all.start_all()
@@ -24,6 +24,7 @@ from .UtilityAndView.User import *  # noqa
 
 session = Session()
 mdb = Mdb()
+version = version_info[0]
 
 backwardCompatibility = BackwardCompatibility()
 
